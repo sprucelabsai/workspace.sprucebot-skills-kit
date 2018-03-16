28a67426872caab0e97dd76420871d5b48e78156
Shane McDermed <shmcdermed@gmail.com>
2018-03-16 09:49:18 -0600

    chore(SB-1112): Allow sentence case for commit messages

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,9 @@
 module.exports = {
     extends: [
         '@commitlint/config-conventional'
-    ]
+    ],
+    rules: {
+        'scope-case': [0],
+        'subject-case': [2, 'always', ['sentence-case', 'lower-case']]
+    }
 }
