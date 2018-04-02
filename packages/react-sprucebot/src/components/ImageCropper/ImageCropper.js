b30d653478bcc25d5ff8e419d1736596b33fb326
Travis Rollins <kalikoze@gmail.com>
2018-04-02 17:27:56 -0600

    feat(SB-792): fix polyfill and web coming in undefined

--- packages/react-sprucebot/src/components/ImageCropper/ImageCropper.js
+++ packages/react-sprucebot/src/components/ImageCropper/ImageCropper.js
@@ -7,7 +7,10 @@ import ReactCrop, { makeAspectCrop, getPixelCrop } from 'react-image-crop'
 import getOrientedImage from 'exif-orientation-image'
 import styled from 'styled-components'
 import SubmitWrapper from '../SubmitWrapper/SubmitWrapper'
-import toBlob from 'blueimp-canvas-to-blob'
+import ExecutionEnvironment from 'exenv'
+if (ExecutionEnvironment.canUseDOM) {
+	require('blueimp-canvas-to-blob')
+}
 
 export default class ImageCropper extends Component {
 	constructor(props) {
