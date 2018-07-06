93e0d7fac29e35a1f94b1ba6c729aebb12bcf73c
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-06 16:17:33 -0600

    chore: Update isDayBlocked in DateRangeSelect

--- packages/react-sprucebot/lib/components/DateRangeSelect/DateRangeSelect.js
+++ packages/react-sprucebot/lib/components/DateRangeSelect/DateRangeSelect.js
@@ -116,9 +116,9 @@ var DateRangeSelect = function (_Component) {
 			var match = availableDays.find(function (day) {
 				return day === date.format('YYYY-MM-DD');
 			});
-			var lastDate = availableDays[availableDays.length - 1];
+			var lastDate = (0, _moment2.default)(availableDays[availableDays.length - 1]).endOf('month');
 
-			if (match || date.isAfter(lastDate)) {
+			if (match || date.isAfter(lastDate) || date.isSame(lastDate)) {
 				return false;
 			}
 			return true;
