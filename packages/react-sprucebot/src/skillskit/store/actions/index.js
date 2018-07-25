1ab1c5a600ec5817ecef5f9f3e609925142105f8
Hugh Hartigan <hartigan.hm@gmail.com>
2018-07-25 16:07:45 -0600

    feat: Add calendar actions, add to index.js

--- packages/react-sprucebot/src/skillskit/store/actions/index.js
+++ packages/react-sprucebot/src/skillskit/store/actions/index.js
@@ -1,6 +1,7 @@
 import * as auth from './auth'
 import * as onboarding from './onboarding'
 import * as sharable from './sharable'
+import * as calendar from './calendar'
 
 module.exports = {
 	auth,
