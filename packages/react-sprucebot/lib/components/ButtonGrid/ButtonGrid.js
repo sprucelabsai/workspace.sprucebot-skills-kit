5d081456f331b4f25d9f6b850144f841dc249b9f
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-15 15:17:33 -0600

    Remove styled-components from ButtonGrid

--- packages/react-sprucebot/lib/components/ButtonGrid/ButtonGrid.js
+++ packages/react-sprucebot/lib/components/ButtonGrid/ButtonGrid.js
@@ -3,17 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
-exports.GridButton = exports.ButtonGrid = undefined;
+exports.GridButton = undefined;
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _templateObject = _taggedTemplateLiteral([''], ['']);
-
-var _styledComponents = require('styled-components');
-
-var _styledComponents2 = _interopRequireDefault(_styledComponents);
 
 var _react = require('react');
 
@@ -32,12 +26,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
-var ButtonGrid = exports.ButtonGrid = _styledComponents2.default.div.attrs({
-	className: 'btn_grid'
-})(_templateObject);
 
 var GridButton = exports.GridButton = function (_Component) {
 	_inherits(GridButton, _Component);
