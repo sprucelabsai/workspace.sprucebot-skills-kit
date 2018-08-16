82d91fcf2eede811b05c12dbe722ff20804b3068
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-16 10:28:59 -0600

    Remove styled-components from ControlButton; build stylesheet

--- packages/react-sprucebot/lib/components/ControlButton/ControlButton.js
+++ packages/react-sprucebot/lib/components/ControlButton/ControlButton.js
@@ -6,14 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _templateObject = _taggedTemplateLiteral(['\n\tmargin-right: 0.25em;\n'], ['\n\tmargin-right: 0.25em;\n']),
-    _templateObject2 = _taggedTemplateLiteral(['\n\tmargin-left: 0.25em;\n'], ['\n\tmargin-left: 0.25em;\n']),
-    _templateObject3 = _taggedTemplateLiteral(['\n\talign-items: center;\n\tbackground: none;\n\tcolor: #00aac7;\n\tdisplay: inline-flex;\n\tfont-weight: normal;\n\tletter-spacing: 0;\n\tposition: relative;\n\tmargin: 0;\n\tpadding: 0;\n\twidth: auto;\n\ttext-decoration: none;\n\n\t&:hover {\n\t\tbackground: none;\n\t}\n'], ['\n\talign-items: center;\n\tbackground: none;\n\tcolor: #00aac7;\n\tdisplay: inline-flex;\n\tfont-weight: normal;\n\tletter-spacing: 0;\n\tposition: relative;\n\tmargin: 0;\n\tpadding: 0;\n\twidth: auto;\n\ttext-decoration: none;\n\n\t&:hover {\n\t\tbackground: none;\n\t}\n']);
-
-var _styledComponents = require('styled-components');
-
-var _styledComponents2 = _interopRequireDefault(_styledComponents);
-
 var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
@@ -30,49 +22,28 @@ var _Icon = require('../Icon/Icon');
 
 var _Icon2 = _interopRequireDefault(_Icon);
 
+require('./ControlButton.scss');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
-var StyledIconLeft = (0, _styledComponents2.default)(_Icon2.default)(_templateObject);
-
-var StyledIconRight = (0, _styledComponents2.default)(_Icon2.default)(_templateObject2);
-
-var StyledButton = (0, _styledComponents2.default)(_Button2.default).attrs({
-	className: function (_className) {
-		function className(_x) {
-			return _className.apply(this, arguments);
-		}
-
-		className.toString = function () {
-			return _className.toString();
-		};
-
-		return className;
-	}(function (_ref) {
-		var className = _ref.className;
-		return 'ControlButton control-button ' + (className || '');
-	})
-})(_templateObject3);
-
-var ControlButton = function ControlButton(_ref2) {
-	var iconLeft = _ref2.iconLeft,
-	    iconRight = _ref2.iconRight,
-	    children = _ref2.children,
-	    props = _objectWithoutProperties(_ref2, ['iconLeft', 'iconRight', 'children']);
+var ControlButton = function ControlButton(_ref) {
+	var iconLeft = _ref.iconLeft,
+	    iconRight = _ref.iconRight,
+	    children = _ref.children,
+	    props = _objectWithoutProperties(_ref, ['iconLeft', 'iconRight', 'children']);
 
 	return _react2.default.createElement(
 		_Button2.default,
 		_extends({}, props, {
-			className: 'ControlButton control-button ' + (className || ''),
+			className: 'ControlButton control__button ' + (className || ''),
 			tabIndex: 0,
 			hideLoader: true
 		}),
 		iconLeft && _react2.default.createElement(
 			_Icon2.default,
-			null,
+			{ className: 'icon__left' },
 			iconLeft
 		),
 		_react2.default.createElement(
@@ -82,7 +53,7 @@ var ControlButton = function ControlButton(_ref2) {
 		),
 		iconRight && _react2.default.createElement(
 			_Icon2.default,
-			null,
+			{ className: 'icon__right' },
 			iconRight
 		)
 	);
--- packages/react-sprucebot/src/components/ControlButton/ControlButton.js
+++ packages/react-sprucebot/src/components/ControlButton/ControlButton.js
@@ -1,49 +1,20 @@
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import React from 'react'
 import Button from '../Button/Button'
 import Icon from '../Icon/Icon'
-
-const StyledIconLeft = styled(Icon)`
-	margin-right: 0.25em;
-`
-
-const StyledIconRight = styled(Icon)`
-	margin-left: 0.25em;
-`
-
-const StyledButton = styled(Button).attrs({
-	className: ({ className }) =>
-		`ControlButton control-button ${className || ''}`
-})`
-	align-items: center;
-	background: none;
-	color: #00aac7;
-	display: inline-flex;
-	font-weight: normal;
-	letter-spacing: 0;
-	position: relative;
-	margin: 0;
-	padding: 0;
-	width: auto;
-	text-decoration: none;
-
-	&:hover {
-		background: none;
-	}
-`
+import './ControlButton.scss'
 
 const ControlButton = ({ iconLeft, iconRight, children, ...props }) => {
 	return (
 		<Button
 			{...props}
-			className={`ControlButton control-button ${className || ''}`}
+			className={`ControlButton control__button ${className || ''}`}
 			tabIndex={0}
 			hideLoader
 		>
-			{iconLeft && <Icon>{iconLeft}</Icon>}
+			{iconLeft && <Icon className={`icon__left`}>{iconLeft}</Icon>}
 			<span>{children}</span>
-			{iconRight && <Icon>{iconRight}</Icon>}
+			{iconRight && <Icon className={`icon__right`}>{iconRight}</Icon>}
 		</Button>
 	)
 }
