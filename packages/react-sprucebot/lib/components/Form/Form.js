db6f2b2c64e557bb6ba989ced64df099ea03c33c
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-24 10:47:41 -0600

    feat: Remove styled components from Form

--- packages/react-sprucebot/lib/components/Form/Form.js
+++ packages/react-sprucebot/lib/components/Form/Form.js
@@ -4,11 +4,11 @@ Object.defineProperty(exports, "__esModule", {
 	value: true
 });
 
-var _templateObject = _taggedTemplateLiteral([''], ['']);
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _styledComponents = require('styled-components');
+var _react = require('react');
 
-var _styledComponents2 = _interopRequireDefault(_styledComponents);
+var _react2 = _interopRequireDefault(_react);
 
 var _classnames = require('classnames');
 
@@ -16,10 +16,13 @@ var _classnames2 = _interopRequireDefault(_classnames);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
-var Form = _styledComponents2.default.form.attrs({
-	className: 'Form'
-})(_templateObject);
+var Form = function Form(_ref) {
+	var className = _ref.className,
+	    props = _objectWithoutProperties(_ref, ['className']);
+
+	return _react2.default.createElement('form', _extends({}, props, { className: 'Form ' + (className || '') }));
+};
 
 exports.default = Form;
