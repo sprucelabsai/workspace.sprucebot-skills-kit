b68efbd9fe6ff07bf3f8f33b25caef507374ff38
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-24 16:15:03 -0600

    feat: Remove styled components from Pager

--- packages/react-sprucebot/lib/components/Pager/Pager.js
+++ packages/react-sprucebot/lib/components/Pager/Pager.js
@@ -8,18 +8,9 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _templateObject = _taggedTemplateLiteral(['\n\tdisplay: flex;\n\talign-items: center;\n\t', ';\n'], ['\n\tdisplay: flex;\n\talign-items: center;\n\t', ';\n']),
-    _templateObject2 = _taggedTemplateLiteral(['\n\t&& {\n\t\t', ';\n\t\t', ';\n\t\t', ';\n\t}\n'], ['\n\t&& {\n\t\t', ';\n\t\t', ';\n\t\t', ';\n\t}\n']),
-    _templateObject3 = _taggedTemplateLiteral(['\n\tmargin-left: 0.7em;\n'], ['\n\tmargin-left: 0.7em;\n']),
-    _templateObject4 = _taggedTemplateLiteral(['\n\t&& {\n\t\tflex: 2;\n\t\tdisplay: flex;\n\t\talign-self: center;\n\t\talign-items: center;\n\t\tjustify-content: center;\n\t}\n'], ['\n\t&& {\n\t\tflex: 2;\n\t\tdisplay: flex;\n\t\talign-self: center;\n\t\talign-items: center;\n\t\tjustify-content: center;\n\t}\n']);
-
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
-
-var _styledComponents = require('styled-components');
-
-var _styledComponents2 = _interopRequireDefault(_styledComponents);
 
 var _propTypes = require('prop-types');
 
@@ -46,24 +37,6 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
-var StyledList = _styledComponents2.default.ul(_templateObject, function (props) {
-	return props.margin && 'margin: ' + props.margin;
-});
-
-var StyledListItem = _styledComponents2.default.li(_templateObject2, function (props) {
-	return props.smallArrows && 'flex: 0.5';
-}, function (props) {
-	return props.hide && 'display: none';
-}, function (props) {
-	return props.loading && '\n\t\t\t\tpointer-events: none;\n\t\t\t\tcursor: not-allowed;\n\t\t\t';
-});
-
-var DropDownButton = (0, _styledComponents2.default)(_ControlButton2.default)(_templateObject3);
-
-var StyledLoader = (0, _styledComponents2.default)(_Loader2.default)(_templateObject4);
 
 var Pager = function (_Component) {
 	_inherits(Pager, _Component);
@@ -170,7 +143,7 @@ var Pager = function (_Component) {
 
 			var title = titles ? titles(page) : page + 1 + ' of ' + totalPages;
 			return _react2.default.createElement(
-				StyledListItem,
+				'li',
 				{ className: 'current' },
 				title
 			);
