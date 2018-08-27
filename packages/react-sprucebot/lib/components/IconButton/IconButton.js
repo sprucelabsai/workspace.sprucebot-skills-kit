306fccd0d0384239a4171c103e1f470e826a3422
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-27 15:11:29 -0600

    chore: Remove styled-component import for IconButton

--- packages/react-sprucebot/lib/components/IconButton/IconButton.js
+++ packages/react-sprucebot/lib/components/IconButton/IconButton.js
@@ -6,12 +6,6 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _templateObject = _taggedTemplateLiteral(['\n\talign-items: center;\n\tbackground: none;\n\tcolor: #00aac7;\n\tdisplay: flex;\n\tfont-size: ', ';\n\tmargin: 0;\n\tpadding: 0;\n\ttext-decoration: none;\n\twidth: auto;\n\n\t&:hover {\n\t\tbackground: none;\n\t}\n'], ['\n\talign-items: center;\n\tbackground: none;\n\tcolor: #00aac7;\n\tdisplay: flex;\n\tfont-size: ', ';\n\tmargin: 0;\n\tpadding: 0;\n\ttext-decoration: none;\n\twidth: auto;\n\n\t&:hover {\n\t\tbackground: none;\n\t}\n']);
-
-var _styledComponents = require('styled-components');
-
-var _styledComponents2 = _interopRequireDefault(_styledComponents);
-
 var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
@@ -31,12 +25,6 @@ var _Button2 = _interopRequireDefault(_Button);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
-
-function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
-
-var StyledButton = (0, _styledComponents2.default)(_Button2.default)(_templateObject, function (props) {
-	return props.fontSize ? '' + props.fontSize : '2em';
-});
 
 var IconButton = function IconButton(_ref) {
 	var children = _ref.children,
