e2b51df949f9135759c277bb39072268ad1cc314
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-28 14:12:06 -0600

    fix: Rewrite Avatar tests

--- packages/react-sprucebot/src/components/Avatar/Avatar.test.js
+++ packages/react-sprucebot/src/components/Avatar/Avatar.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
-import 'jest-styled-components'
-
 import Avatar from './Avatar'
+import { shallow } from 'enzyme'
 
-test('it renders', () => {
-	const tree = renderer.create(<Avatar image="test.jpg" />).toJSON()
-	expect(tree).toMatchSnapshot()
+describe('Avatar tests', () => {
+	it('Should match the snapshot', () => {
+		const renderedComponent = shallow(<Avatar />)
+
+		expect(renderedComponent).toMatchSnapshot()
+	})
 })
