11089d2350c53b86c1c5c0aa1a8ccc6e72a079cc
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-28 14:36:46 -0600

    chore: Rewrite Form tests

--- packages/react-sprucebot/lib/components/Form/Form.test.js
+++ packages/react-sprucebot/lib/components/Form/Form.test.js
@@ -4,23 +4,18 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactTestRenderer = require('react-test-renderer');
-
-var _reactTestRenderer2 = _interopRequireDefault(_reactTestRenderer);
-
-require('jest-styled-components');
-
 var _Form = require('./Form');
 
 var _Form2 = _interopRequireDefault(_Form);
 
+var _enzyme = require('enzyme');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-test('it renders', function () {
-	var tree = _reactTestRenderer2.default.create(_react2.default.createElement(
-		_Form2.default,
-		null,
-		'Form children'
-	)).toJSON();
-	expect(tree).toMatchSnapshot();
+describe('Form tests', function () {
+	it('Should match the snapshot', function () {
+		var renderedComponent = (0, _enzyme.shallow)(_react2.default.createElement(_Form2.default, null));
+
+		expect(renderedComponent).toMatchSnapshot();
+	});
 });
