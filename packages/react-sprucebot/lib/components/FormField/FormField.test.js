e08429d5243c254b60d3e5b7d42406b204e80f95
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-28 14:37:50 -0600

    chore: Rewrite FormField tests

--- packages/react-sprucebot/lib/components/FormField/FormField.test.js
+++ packages/react-sprucebot/lib/components/FormField/FormField.test.js
@@ -4,45 +4,18 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _enzyme = require('enzyme');
-
-require('jest-styled-components');
-
 var _FormField = require('./FormField');
 
 var _FormField2 = _interopRequireDefault(_FormField);
 
+var _enzyme = require('enzyme');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var onChange = jest.fn();
+describe('FormField tests', function () {
+	it('Should match the snapshot', function () {
+		var renderedComponent = (0, _enzyme.shallow)(_react2.default.createElement(_FormField2.default, null));
 
-test('it renders set value and triggers onChange', function () {
-	var wrapper = (0, _enzyme.shallow)(_react2.default.createElement(
-		_FormField2.default,
-		{
-			type: 'text',
-			input: { value: 'test1', onChange: onChange },
-			meta: { touched: false }
-		},
-		_react2.default.createElement('input', null)
-	));
-	expect(wrapper).toMatchSnapshot();
-	wrapper.find('input[type="text"]').simulate('change', { target: { value: 'test' } });
-	expect(onChange).toHaveBeenCalledWith({ target: { value: 'test' } });
-});
-
-test('it renders with a label, finePrint, placeholder, and error', function () {
-	var wrapper = (0, _enzyme.shallow)(_react2.default.createElement(
-		_FormField2.default,
-		{
-			label: 'Test Label',
-			placeholder: 'Placeholdn\'',
-			type: 'text',
-			input: { value: 'test2', onChange: onChange },
-			meta: { touched: true, error: 'TEST ERROR' },
-			finePrint: 'Fine Print!'
-		},
-		_react2.default.createElement('input', null)
-	));
-	expect(wrapper).toMatchSnapshot();
+		expect(renderedComponent).toMatchSnapshot();
+	});
 });
--- packages/react-sprucebot/src/components/FormField/FormField.test.js
+++ packages/react-sprucebot/src/components/FormField/FormField.test.js
@@ -1,39 +1,11 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import 'jest-styled-components'
-
 import FormField from './FormField'
-const onChange = jest.fn()
+import { shallow } from 'enzyme'
 
-test('it renders set value and triggers onChange', () => {
-	const wrapper = shallow(
-		<FormField
-			type="text"
-			input={{ value: 'test1', onChange }}
-			meta={{ touched: false }}
-		>
-			<input />
-		</FormField>
-	)
-	expect(wrapper).toMatchSnapshot()
-	wrapper
-		.find('input[type="text"]')
-		.simulate('change', { target: { value: 'test' } })
-	expect(onChange).toHaveBeenCalledWith({ target: { value: 'test' } })
-})
+describe('FormField tests', () => {
+	it('Should match the snapshot', () => {
+		const renderedComponent = shallow(<FormField />)
 
-test('it renders with a label, finePrint, placeholder, and error', () => {
-	const wrapper = shallow(
-		<FormField
-			label="Test Label"
-			placeholder="Placeholdn'"
-			type="text"
-			input={{ value: 'test2', onChange }}
-			meta={{ touched: true, error: 'TEST ERROR' }}
-			finePrint="Fine Print!"
-		>
-			<input />
-		</FormField>
-	)
-	expect(wrapper).toMatchSnapshot()
+		expect(renderedComponent).toMatchSnapshot()
+	})
 })
