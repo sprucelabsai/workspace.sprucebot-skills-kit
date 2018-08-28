d5d2412a775436ad55349a7e60b00a878a5e51ca
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-28 14:36:04 -0600

    chore: Rewrite ControlButton tests

--- packages/react-sprucebot/lib/components/ControlButton/ControlButton.test.js
+++ packages/react-sprucebot/lib/components/ControlButton/ControlButton.test.js
@@ -4,26 +4,18 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactTestRenderer = require('react-test-renderer');
-
-var _reactTestRenderer2 = _interopRequireDefault(_reactTestRenderer);
-
-require('jest-styled-components');
-
 var _ControlButton = require('./ControlButton');
 
 var _ControlButton2 = _interopRequireDefault(_ControlButton);
 
+var _enzyme = require('enzyme');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-test('it renders', function () {
-	var tree = _reactTestRenderer2.default.create(_react2.default.createElement(_ControlButton2.default, null)).toJSON();
-	expect(tree).toMatchSnapshot();
-});
+describe('ControlButton tests', function () {
+	it('Should match the snapshot', function () {
+		var renderedComponent = (0, _enzyme.shallow)(_react2.default.createElement(_ControlButton2.default, null));
 
-// test('it renders a link', () => {
-// 	const tree = renderer
-// 		.create(<ControlButton href="https://sprucebot.com" />)
-// 		.toJSON()
-// 	expect(tree).toMatchSnapshot()
-// })
+		expect(renderedComponent).toMatchSnapshot();
+	});
+});
