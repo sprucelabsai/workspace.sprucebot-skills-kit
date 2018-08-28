032e47808d6bb2514ff7e776f1dafbefd2ff0eac
Hugh Hartigan <hartigan.hm@gmail.com>
2018-08-28 14:48:03 -0600

    chore: Rewrite LinkPile tests

--- packages/react-sprucebot/lib/components/LinkPile/LinkPile.test.js
+++ packages/react-sprucebot/lib/components/LinkPile/LinkPile.test.js
@@ -4,19 +4,18 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactTestRenderer = require('react-test-renderer');
-
-var _reactTestRenderer2 = _interopRequireDefault(_reactTestRenderer);
-
-require('jest-styled-components');
-
 var _LinkPile = require('./LinkPile');
 
 var _LinkPile2 = _interopRequireDefault(_LinkPile);
 
+var _enzyme = require('enzyme');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-test('it renders', function () {
-	var tree = _reactTestRenderer2.default.create(_react2.default.createElement(_LinkPile2.default, null)).toJSON();
-	expect(tree).toMatchSnapshot();
+describe('LinkPile tests', function () {
+	it('Should match the snapshot', function () {
+		var renderedComponent = (0, _enzyme.shallow)(_react2.default.createElement(_LinkPile2.default, null));
+
+		expect(renderedComponent).toMatchSnapshot();
+	});
 });
