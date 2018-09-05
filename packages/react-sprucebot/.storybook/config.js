7ded2651f2ecab6f149400f16f2ae43468905395
Brandon Minch <brandon@barbershop.io>
2018-09-05 10:08:58 -0600

    fix: add babel-polyfill to storybook config

--- packages/react-sprucebot/.storybook/config.js
+++ packages/react-sprucebot/.storybook/config.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from 'react'
+import 'babel-polyfill'
 import 'storybook-readme/register'
 import { addDecorator } from '@storybook/react'
 import { setOptions } from '@storybook/addon-options'
 import { setDefaults } from '@storybook/addon-info'
 import backgrounds from '@storybook/addon-backgrounds'
-import Container from './Container';
+import Container from './Container'
 
 // addon-info
 setDefaults({
@@ -51,7 +52,7 @@ addDecorator(
 )
 
 // Container
-addDecorator(story => <Container story={story} />);
+addDecorator(story => <Container story={story} />)
 
 setOptions({
 	name: 'react-sprucebot',
