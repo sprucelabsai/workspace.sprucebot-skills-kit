297581056f21dddbc9ed6083a9ea23479c051e3c
jtini <jermtee@gmail.com>
2018-10-05 13:18:21 -0600

    major: update exports

--- packages/react-sprucebot/src/Sprucebot.js
+++ packages/react-sprucebot/src/Sprucebot.js
@@ -1,5 +1,23 @@
 // @flow
 import Button from './components/Button/Button'
+import ButtonGroup from './components/ButtonGroup/ButtonGroup'
+import * as Card from './components/Card/Card'
+import ContextMenu from './components/ContextMenu/ContextMenu'
+import Dropzone from './components/Dropzone/Dropzone'
+import * as FormPartials from './components/Forms/FormPartials'
+import Checkbox from './components/Forms/Checkbox/Checkbox'
+import DomainInput from './components/Forms/DomainInput/DomainInput'
+import PhoneInput from './components/Forms/PhoneInput/PhoneInput'
+import Radio from './components/Forms/Radio/Radio'
+import Search from './components/Forms/Search/Search'
+import Select from './components/Forms/Select/Select'
+import Slider from './components/Forms/Slider/Slider'
+import Tag from './components/Forms/Tag/Tag'
+import TextArea from './components/Forms/TextArea/TextArea'
+import TextInput from './components/Forms/TextInput/TextInput'
+import Toggle from './components/Forms/Toggle/Toggle'
+import Container from './components/Layout/Container/Container'
+import Loader from './components/Forms/Loader/Loader'
 import skill from './skillskit'
 import _document from './skillskit/next/_document'
 import Page from './skillskit/next/Page'
@@ -31,6 +49,24 @@ const Sprucebot = {
 	Page,
 	withStore,
 	sharable,
-	Button
+	Button,
+	ButtonGroup,
+	...Card,
+	ContextMenu,
+	Dropzone,
+	...FormPartials,
+	Checkbox,
+	DomainInput,
+	PhoneInput,
+	Radio,
+	Search,
+	Select,
+	Slider,
+	Tag,
+	TextArea,
+	TextInput,
+	Toggle,
+	Container,
+	Loader
 }
 module.exports = Sprucebot
