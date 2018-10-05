85d89423060cfa8e66e9685319b0ed22f61abc97
jtini <jermtee@gmail.com>
2018-10-05 08:54:37 -0600

    docs: add heartwood

--- packages/react-sprucebot/.storybook/Wrapper.js
+++ packages/react-sprucebot/.storybook/Wrapper.js
@@ -2,17 +2,21 @@ import React from 'react'
 import Helmet from 'react-helmet'
 // import '../src/stylesheets/vendor.sass'
 // import '../src/stylesheets/global.sass'
-// import '@sprucelabs/heartwood-components/stylesheets/global.scss';
+import '@sprucelabs/heartwood-components/stylesheets/global.scss'
 
 const Wrapper = ({ children }) => {
 	const stylesheets = process.env.STYLESHEETS.split(',')
+
 	return (
 		<div className="l-page-wrapper">
-			<Helmet>
-				{stylesheets.map((stylesheet, idx) => (
-					<link key={idx} rel="stylesheet" href={stylesheet} />
-				))}
-			</Helmet>
+			{stylesheets &&
+				stylesheets.length > 0 && (
+					<Helmet>
+						{stylesheets.map((stylesheet, idx) => (
+							<link key={idx} rel="stylesheet" href={stylesheet} />
+						))}
+					</Helmet>
+				)}
 			<div className="l-pa-medium">{children}</div>
 		</div>
 	)
