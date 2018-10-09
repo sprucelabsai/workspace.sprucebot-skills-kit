330b8001b0fda2270c699f5ee9f0a0651773bfa3
jtini <jermtee@gmail.com>
2018-10-09 15:32:23 -0600

    minor: update indices

--- packages/react-sprucebot/src/Sprucebot.js
+++ packages/react-sprucebot/src/Sprucebot.js
@@ -1,23 +1,33 @@
 // @flow
+
+import Avatar from './components/Avatar/Avatar'
 import Button from './components/Button/Button'
 import ButtonGroup from './components/ButtonGroup/ButtonGroup'
 import * as Card from './components/Card/Card'
 import ContextMenu from './components/ContextMenu/ContextMenu'
+import { FooterPrimary, HeaderPrimary, Sidebar } from './components/Core'
 import Dropzone from './components/Dropzone/Dropzone'
 import * as FormPartials from './components/Forms/FormPartials'
-import Checkbox from './components/Forms/Checkbox/Checkbox'
-import DomainInput from './components/Forms/DomainInput/DomainInput'
-import PhoneInput from './components/Forms/PhoneInput/PhoneInput'
-import Radio from './components/Forms/Radio/Radio'
-import Search from './components/Forms/Search/Search'
-import Select from './components/Forms/Select/Select'
-import Slider from './components/Forms/Slider/Slider'
-import Tag from './components/Forms/Tag/Tag'
-import TextArea from './components/Forms/TextArea/TextArea'
-import TextInput from './components/Forms/TextInput/TextInput'
-import Toggle from './components/Forms/Toggle/Toggle'
+import {
+	Checkbox,
+	DomainInput,
+	PhoneInput,
+	Radio,
+	Search,
+	Select,
+	Slider,
+	Tag,
+	TextArea,
+	TextInput,
+	Toggle
+} from './components/Forms'
 import Container from './components/Layout/Container/Container'
+import List, { ListHeader, ListItem, SortableList } from './components/List'
 import Loader from './components/Forms/Loader/Loader'
+import Modal from './components/Modal/Modal'
+import Tabs, { Tab } from './components/Tab'
+import Toast from './components/Toast/Toast'
+import View from './components/View/View'
 import skill from './skillskit'
 import _document from './skillskit/next/_document'
 import Page from './skillskit/next/Page'
@@ -49,24 +59,37 @@ const Sprucebot = {
 	Page,
 	withStore,
 	sharable,
+	Avatar,
 	Button,
 	ButtonGroup,
 	...Card,
-	ContextMenu,
-	Dropzone,
-	...FormPartials,
 	Checkbox,
+	Container,
+	ContextMenu,
 	DomainInput,
+	Dropzone,
+	FooterPrimary,
+	...FormPartials,
+	HeaderPrimary,
+	List,
+	ListHeader,
+	ListItem,
+	Loader,
+	Modal,
 	PhoneInput,
 	Radio,
 	Search,
 	Select,
+	Sidebar,
 	Slider,
+	SortableList,
+	Tabs,
+	Tab,
 	Tag,
 	TextArea,
 	TextInput,
+	Toast,
 	Toggle,
-	Container,
-	Loader
+	View
 }
 module.exports = Sprucebot
---
+++ packages/react-sprucebot/src/components/Core/index.js
@@ -0,0 +1,7 @@
+import FooterPrimary from './components/FooterPrimary/FooterPrimary'
+import HeaderPrimary from './components/HeaderPrimary/HeaderPrimary'
+import Sidebar from './components/Sidebar/Sidebar'
+
+export FooterPrimary from './components/FooterPrimary/FooterPrimary'
+export HeaderPrimary from './components/HeaderPrimary/HeaderPrimary'
+export Sidebar from './components/Sidebar/Sidebar'
