dde8c5c761954d6d1fc4d368aa89fe9fbe5d1f0b
jtini <jermtee@gmail.com>
2018-10-24 13:27:21 -0600

    minor: build

---
+++ packages/react-sprucebot/lib/components/BigCalendar/components/EventDetails/components/EventDetailsFooter/EventDetailsFooter.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _extends2 = _interopRequireDefault(require("@babel/runtime/helpers/extends"));
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Button = _interopRequireWildcard(require("../../../../../Button/Button"));
+
+var EventDetailsFooter = function EventDetailsFooter(props) {
+  var primaryCTA = props.primaryCTA;
+  return _react.default.createElement("div", {
+    className: "event-details-footer"
+  }, _react.default.createElement(_Button.default, (0, _extends2.default)({
+    kind: "primary",
+    isFullWidth: true
+  }, primaryCTA)));
+};
+
+var _default = EventDetailsFooter;
+exports.default = _default;
