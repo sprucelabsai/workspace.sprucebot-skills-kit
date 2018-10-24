a9dbb15514a936620aaa87414e52f79d18c8c81b
Ken Goldfarb <hello@kengoldfarb.com>
2018-10-24 13:05:36 -0600

    test: mock global logger

--- packages/sprucebot-skills-kit/jest/enzymeSetup.js
+++ packages/sprucebot-skills-kit/jest/enzymeSetup.js
@@ -2,3 +2,15 @@ import 'raf/polyfill'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 Enzyme.configure({ adapter: new Adapter() })
+
+// Mock the global logger that is always available
+global.log = {
+	trace: () => console.log('trace', arguments),
+	debug: () => console.log('debug', arguments),
+	info: () => console.log('info', arguments),
+	warn: () => console.log('warn', arguments),
+	crit: () => console.log('crit', arguments),
+	error: () => console.log('error', arguments),
+	superInfo: () => console.log('superInfo', arguments),
+	metric: () => console.log('metric', arguments)
+}
