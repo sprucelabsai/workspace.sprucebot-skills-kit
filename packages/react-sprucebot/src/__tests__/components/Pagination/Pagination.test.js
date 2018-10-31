435b13a2d5e3a82214956a47e90aa8eb031325cd
CarolynSM <carolyn@barbershop.io>
2018-10-31 13:51:57 -0600

    pagination tests

--- packages/react-sprucebot/src/__tests__/components/Pagination/Pagination.test.js
+++ packages/react-sprucebot/src/__tests__/components/Pagination/Pagination.test.js
@@ -1,5 +1,6 @@
+import 'jsdom-global/register'
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import Pagination from '../../../components/Pagination/Pagination'
 
@@ -9,45 +10,31 @@ describe('Pagination tests', () => {
 	let backButton
 	let onClickNext
 	let onClickBack
+	let props
 
 	beforeEach(() => {
 		onClickNext = jest.fn()
 		onClickBack = jest.fn()
 
-		renderedComponent = shallow(
-			<Pagination
-				isSimple={true}
-				onClickNext={onClickNext}
-				onClickBack={onClickBack}
-				currentPage={2}
-				totalPages={3}
-			/>
-		)
+		props = {
+			isSimple: true,
+			onClickNext: onClickNext,
+			onClickBack: onClickBack,
+			currentPage: 0,
+			totalPages: 3
+		}
+
+		renderedComponent = shallow(<Pagination {...props} />)
 
 		nextButton = renderedComponent.find('Button').at(1)
 		backButton = renderedComponent.find('Button').at(0)
 	})
 
 	it('Renders', () => {
+		expect(renderedComponent).toBeDefined()
 		expect(renderedComponent.exists()).toEqual(true)
 		expect(nextButton.exists()).toEqual(true)
-		expect(nextButton.prop('icon')).toEqual(
-			<ArrowNext
-				height="24"
-				viewBox="0 0 24 24"
-				width="24"
-				xmlns="http://www.w3.org/2000/svg"
-			/>
-		)
 		expect(backButton.exists()).toEqual(true)
-		expect(backButton.prop('icon')).toEqual(
-			<ArrowBack
-				height="24"
-				viewBox="0 0 24 24"
-				width="24"
-				xmlns="http://www.w3.org/2000/svg"
-			/>
-		)
 	})
 
 	it(`Calls the 'click next' handler`, () => {
@@ -59,53 +46,98 @@ describe('Pagination tests', () => {
 		backButton.simulate('click')
 		expect(onClickBack).toHaveBeenCalled()
 	})
+})
 
-	describe('When on the first page', () => {
-		renderedComponent = shallow(
-			<Pagination
-				isSimple={true}
-				onClickNext={onClickNext}
-				onClickBack={onClickBack}
-				currentPage={1}
-				totalPages={3}
-			/>
-		)
+describe('When on the first page', () => {
+	let renderedComponent
+	let nextButton
+	let backButton
+	let onClickNext
+	let onClickBack
+	let props
 
-		it('Back buttons is disabled', () => {
-			console.log(backButton.debug())
-			expect(backButton.prop('disabled')).toEqual(true)
-			expect(nextButton.prop('disabled')).toEqual(false)
-		})
+	beforeEach(() => {
+		onClickNext = jest.fn()
+		onClickBack = jest.fn()
+
+		props = {
+			isSimple: true,
+			onClickNext: onClickNext,
+			onClickBack: onClickBack,
+			currentPage: 0,
+			totalPages: 3
+		}
+
+		renderedComponent = shallow(<Pagination {...props} />)
+
+		nextButton = renderedComponent.find('Button').at(1)
+		backButton = renderedComponent.find('Button').at(0)
 	})
 
-	describe('When between the first and last page', () => {
-		renderedComponent = shallow(
-			<Pagination
-				isSimple={true}
-				onClickNext={onClickNext}
-				onClickBack={onClickBack}
-				currentPage={2}
-				totalPages={3}
-			/>
-		)
-		it('Both pager buttons are enabled', () => {
-			expect(backButton.prop('disabled')).toEqual(false)
-			expect(nextButton.prop('disabled')).toEqual(false)
-		})
+	it('Back buttons is disabled', () => {
+		expect(backButton.prop('disabled')).toEqual(true)
+		expect(nextButton.prop('disabled')).toEqual(false)
 	})
-	describe('When on the last page', () => {
-		renderedComponent = shallow(
-			<Pagination
-				isSimple={true}
-				onClickNext={onClickNext}
-				onClickBack={onClickBack}
-				currentPage={3}
-				totalPages={3}
-			/>
-		)
-		it('Next button is disabled', () => {
-			expect(backButton.prop('disabled')).toEqual(false)
-			expect(nextButton.prop('disabled')).toEqual(true)
-		})
+})
+
+describe('When between the first and last page', () => {
+	let renderedComponent
+	let nextButton
+	let backButton
+	let onClickNext
+	let onClickBack
+	let props
+
+	beforeEach(() => {
+		onClickNext = jest.fn()
+		onClickBack = jest.fn()
+
+		props = {
+			isSimple: true,
+			onClickNext: onClickNext,
+			onClickBack: onClickBack,
+			currentPage: 1,
+			totalPages: 3
+		}
+
+		renderedComponent = shallow(<Pagination {...props} />)
+
+		nextButton = renderedComponent.find('Button').at(1)
+		backButton = renderedComponent.find('Button').at(0)
+	})
+	it('Both pager buttons are enabled', () => {
+		expect(backButton.prop('disabled')).toEqual(false)
+		expect(nextButton.prop('disabled')).toEqual(false)
+	})
+})
+
+describe('When on the last page', () => {
+	let renderedComponent
+	let nextButton
+	let backButton
+	let onClickNext
+	let onClickBack
+	let props
+
+	beforeEach(() => {
+		onClickNext = jest.fn()
+		onClickBack = jest.fn()
+
+		props = {
+			isSimple: true,
+			onClickNext: onClickNext,
+			onClickBack: onClickBack,
+			currentPage: 3,
+			totalPages: 3
+		}
+
+		renderedComponent = shallow(<Pagination {...props} />)
+
+		nextButton = renderedComponent.find('Button').at(1)
+		backButton = renderedComponent.find('Button').at(0)
+	})
+	it('Next button is disabled', () => {
+		expect(backButton.prop('disabled')).toEqual(false)
+		expect(nextButton.prop('disabled')).toEqual(true)
 	})
 })
