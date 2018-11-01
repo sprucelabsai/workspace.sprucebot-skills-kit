2311df29c80fc92fa318a224c28ddddac2e9ce99
CarolynSM <carolyn@barbershop.io>
2018-11-01 13:17:50 -0600

    passing tests for big calendar

--- packages/react-sprucebot/src/__tests__/components/BigCalendar/Day.test.js
+++ packages/react-sprucebot/src/__tests__/components/BigCalendar/Day.test.js
@@ -1,28 +1,41 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import 'jsdom-global/register'
+import { mount } from 'enzyme'
 import moment from 'moment'
 
 import Day from '../../../components/BigCalendar/components/Views/Day'
+
 import users from '../../../__mocks__/stubs/users'
 import location from '../../../__mocks__/stubs/location'
+import events from '../../../__mocks__/stubs/events'
 
 describe('Day View tests', () => {
 	let renderedComponent
+	let props
 	let teammateHeader
 	let timeGutter
 
 	beforeEach(() => {
-		renderedComponent = shallow(
-			<Day
-				onScroll={() => {}}
-				viewHeight={0}
-				hours={[]}
-				users={users}
-				location={location}
-				minTime={'00:00'}
-				maxTime={'23:59'}
-			/>
-		)
+		props = {
+			onScroll: jest.fn(),
+			viewHeight: 0,
+			hours: [],
+			users: users,
+			location: location,
+			minTime: '00:00',
+			maxTime: '23:59',
+			events: events,
+			dragThreshold: 10,
+			showRightProps: false,
+			startTime: '07:00:00',
+			endTime: '20:00:00',
+			slotsPerHour: 4,
+			onUpdateHorizontalPagerDetails: jest.fn(),
+			startDate: moment(),
+			onDropEvent: jest.fn()
+		}
+
+		renderedComponent = mount(<Day {...props} />)
 
 		teammateHeader = renderedComponent.find('TeammateHeader')
 		timeGutter = renderedComponent.find('TimeGutter')
