6e923a0415e388dbecd0dfa12e9dcaf8b10af5c3
Ken Goldfarb <hello@kengoldfarb.com>
2018-11-09 16:19:02 -0700

    update path

--- packages/react-heartwood-components/.storybook/StylesProvider.js
+++ packages/react-heartwood-components/.storybook/StylesProvider.js
@@ -2,8 +2,7 @@ import React, { Fragment } from 'react'
 import Helmet from 'react-helmet'
 import '../src/stylesheets/vendor.sass'
 import '../src/stylesheets/global.sass'
-// import '@sprucelabs/heartwood-components/stylesheets/global.scss'
-import '../../sprucebot-heartwood/stylesheets/global.scss'
+import '../../heartwood-components/stylesheets/global.scss'
 
 const StylesProvider = ({ children }) => {
 	const stylesheets =
