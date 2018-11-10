e2768ec9cd52ef0e33bdd1de6b938b763d1b6d12
Brandon Minch <brandon@barbershop.io>
2018-11-10 15:24:24 -0700

    update build process

--- packages/heartwood-components/Gulpfile.js
+++ packages/heartwood-components/Gulpfile.js
@@ -1,31 +1,39 @@
-var gulp = require('gulp');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var sassGlob = require('gulp-sass-glob');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var rename = require("gulp-rename");
+var sassGlob = require("gulp-sass-glob");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+var sourcemaps = require("gulp-sourcemaps");
 
 // a task to import base variables first, then from within our import file, fetch component styles and add them to a concatenated output file
-gulp.task('styles', function() {
-	gulp.src(['stylesheets/global.scss'])
-	.pipe(sassGlob())
-	.pipe(sass().on('error', sass.logError))
-	// .pipe(rename('site.css'))
-	// this is where site.css will be output. You'll reference this file in your _preview.hbs, so make sure the location is located in the static asset folder defined in fractal.js
-	.pipe(gulp.dest('./public/stylesheets'))
+gulp.task("styles", function() {
+  gulp
+    .src(["stylesheets/heartwood-components.scss"])
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer]))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./public/stylesheets"))
+    .pipe(rename({ extension: ".min.css" }))
+    .pipe(gulp.dest("./public/stylesheets"));
 });
 
-gulp.task('js', function() {
-	gulp.src(['components/**/*.js', '!components/**/*.config.js'])
-	// .pipe(concat('scripts.js'))
-	.pipe(gulp.dest('./public/js'))
-})
+gulp.task("js", function() {
+  gulp
+    .src(["components/**/*.js", "!components/**/*.config.js"])
+    .pipe(gulp.dest("./public/js"));
+});
 
 // watcher
-gulp.task('watch', function(){
-	// don't watch our import file for changes, watch the underlying partials for changes. If changes, run styles task to re-compile
-	gulp.watch(['stylesheets/**/*.scss', 'components/**/*.scss', 'components/**/*.js'] , ['styles', 'js']);
-})
+gulp.task("watch", function() {
+  // don't watch our import file for changes, watch the underlying partials for changes. If changes, run styles task to re-compile
+  gulp.watch(
+    ["stylesheets/**/*.scss", "components/**/*.scss", "components/**/*.js"],
+    ["styles", "js"]
+  );
+});
 
 // make sure you run fractal with "fractal start --sync" to use livereload in conjunction with this
