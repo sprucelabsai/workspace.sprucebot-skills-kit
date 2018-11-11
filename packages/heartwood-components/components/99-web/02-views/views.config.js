fc02619227f1a4bc21b96b3ff9f089ffece93cc5
Brandon Minch <brandon@barbershop.io>
2018-11-10 19:30:09 -0700

    reorganize web and view components

--- packages/heartwood-components/components/99-web/02-views/views.config.js
+++ packages/heartwood-components/components/99-web/02-views/views.config.js
@@ -2,7 +2,7 @@ const image = 'https://images.unsplash.com/photo-1524593689594-aae2f26b75ab?ixli
 
 
 module.exports = {
-	title: 'Layouts',
+	title: 'Views',
 	context: {
 		openIcon: '<path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>',
 		closeIcon: '<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/>'
