745a3090c4eded16ca79cd56f8181f6e7247d9f4
Brandon Minch <brandon@barbershop.io>
2018-11-12 11:36:22 -0700

    flatten layout structure

--- packages/heartwood-components/components/03-structure/03-layout/layout.config.js
+++ packages/heartwood-components/components/03-structure/03-layout/layout.config.js
@@ -1,6 +1,12 @@
 module.exports = {
-	title: 'Layout Sections',
+	title: 'Layout',
 	status: 'wip',
+	collated: true,
+	collator: function(markup, item) {
+		return `<!-- Start: @${item.handle} -->\n<h2 style="margin: 3rem 0;">${
+			item.title
+		}</h2><div>${markup}</div>\n<!-- End: @${item.handle} -->\n`
+	},
 	context: {
 		sections: [
 			{
@@ -34,41 +40,6 @@ module.exports = {
 		]
 	},
 	variants: [
-		{
-			name: '2 Columns Primary and Secondary Width',
-			context: {
-				sections: [
-					{
-						secondary: false,
-						card: {
-							title: 'Get the most out of Sprucebot',
-							children:
-								'<div class="card__body-inner">Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.</div>',
-							footerActions: [
-								{
-									text: 'Get some skills',
-									className: 'btn-secondary btn-small'
-								}
-							]
-						}
-					},
-					{
-						secondary: true,
-						card: {
-							title: 'Get the most out of Sprucebot',
-							children:
-								'<div class="card__body-inner">Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.</div>',
-							footerActions: [
-								{
-									text: 'Get some skills',
-									className: 'btn-secondary btn-small'
-								}
-							]
-						}
-					}
-				]
-			}
-		},
 		{
 			name: '2 Columns Equal Width',
 			context: {
@@ -124,6 +95,41 @@ module.exports = {
 					},
 					{
 						secondary: true,
+						card: {
+							title: 'Get the most out of Sprucebot',
+							children:
+								'<div class="card__body-inner">Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.</div>',
+							footerActions: [
+								{
+									text: 'Get some skills',
+									className: 'btn-secondary btn-small'
+								}
+							]
+						}
+					},
+					{
+						secondary: true,
+						card: {
+							title: 'Get the most out of Sprucebot',
+							children:
+								'<div class="card__body-inner">Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.</div>',
+							footerActions: [
+								{
+									text: 'Get some skills',
+									className: 'btn-secondary btn-small'
+								}
+							]
+						}
+					}
+				]
+			}
+		},
+		{
+			name: '2 Columns Primary and Secondary Width',
+			context: {
+				sections: [
+					{
+						secondary: false,
 						card: {
 							title: 'Get the most out of Sprucebot',
 							children:
