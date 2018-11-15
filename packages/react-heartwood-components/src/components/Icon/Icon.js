5ec6144aae21a886f3180c79f52b5b383d0e1896
taylorromero <me>
2018-11-15 14:40:13 -0700

    icons

--- packages/react-heartwood-components/src/components/Icon/Icon.js
+++ packages/react-heartwood-components/src/components/Icon/Icon.js
@@ -8,7 +8,14 @@ import Add from '../../../static/assets/icons/ic_add_circle_outline.svg'
 import CalendarDate from '../../../static/assets/icons/Interface-Essential/Date/Calendar/calendar-date.svg'
 import Close from '../../../static/assets/icons/ic_close.svg'
 import Delete from '../../../static/assets/icons/Interface-Essential/Delete/bin.svg'
-import Edit from '../../../static/assets//icons/Interface-Essential/Edit/pencil-write.svg'
+import Edit from '../../../static/assets/icons/Interface-Essential/Edit/pencil-write.svg'
+import RadioUnchecked from '../../../static/assets/icons/ic_radio_button_unchecked.svg'
+import PaymentFailedSolid from '../../../static/assets/icons/ic_payment_failed_solid.svg'
+import PaymentSuccessSolid from '../../../static/assets/icons/ic_payment_success_solid.svg'
+import CheckCircle from '../../../static/assets/icons/ic_check_circle.svg'
+import Late from '../../../static/assets/icons/ic_late.svg'
+import Disable from '../../../static/assets/icons/ic_disable.svg'
+import Teammates from '../../../static/assets/icons/ic_teammates.svg'
 
 type Props = {
 	/** The name of the icon to render. If not found, this will return null. */
@@ -23,15 +30,21 @@ const key = {
 	close: Close,
 	date: CalendarDate,
 	delete: Delete,
-	edit: Edit
+	edit: Edit,
+	radio_button_unchecked: RadioUnchecked,
+	payment_failed_solid: PaymentFailedSolid,
+	checked_cicrle: CheckCircle,
+	late: Late,
+	payment_success_solid: PaymentSuccessSolid,
+	disable: Disable,
+	teammates: Teammates
 }
 
 const Icon = (props: Props) => {
 	const { icon, customIcon, isLineIcon, className, ...rest } = props
 
 	if (!customIcon && (!icon || !key[icon.toLowerCase()])) {
-		// TODO: handle logging
-		// console.error(`<Icon /> could not find an icon with key ${icon}`)
+		log.warn(`<Icon /> could not find an icon with key ${icon}`)
 		return null
 	}
 	const Handler = customIcon || key[icon.toLowerCase()]
