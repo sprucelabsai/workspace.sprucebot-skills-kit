96fe08cbdb5b019f0da1fb800e5a073cca4888ad
George Pantazis <gcpantazis@gmail.com>
2018-11-16 09:16:28 -0600

    Add a demo page showing all icons simultaneously

--- packages/react-heartwood-components/src/components/Icon/Icon-story.js
+++ packages/react-heartwood-components/src/components/Icon/Icon-story.js
@@ -1,12 +1,14 @@
 // @flow
 import React from 'react'
-import { each, keys } from 'lodash'
+import { each, keys, map } from 'lodash'
 import cx from 'classnames'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean, select } from '@storybook/addon-knobs/react'
 import Icon from './Icon'
 
 import * as icons from '../../icons.js'
+
+import './demo.scss'
 
 const options = {}
 
@@ -23,4 +25,15 @@ stories.add('Icon', () => (
 		icon={select('icon', options, 'edit')}
 		isLineIcon={boolean('isLineIcon', true)}
 	/>
+))
+
+stories.add('All Icons', () => (
+	<div className="Storybook-IconGrid">
+		{map(keys(icons), icon => (
+			<div className="Storybook-IconGridItem" key={`icon-${icon}`}>
+				<Icon icon={icon} isLineIcon={boolean('isLineIcon', true)} />
+				<div>{icon}</div>
+			</div>
+		))}
+	</div>
 ))
