7bf01f4342dd1a3a49debf678bf5dcbfadd47311
CarolynSM <carolyn@barbershop.io>
2018-12-19 15:25:49 -0700

    make <View> a stateless functional component

--- packages/react-heartwood-components/src/components/View/View.js
+++ packages/react-heartwood-components/src/components/View/View.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react'
+import React from 'react'
 import type { Node } from 'react'
 import cx from 'classnames'
 import HeaderPrimary from '../Core/components/HeaderPrimary/HeaderPrimary'
@@ -12,89 +12,59 @@ type Props = {
 	getSearchSuggestionValue?: Function,
 	renderSearchSuggestion?: Function,
 	children: Node,
-	toggleSidebarVisibility?: Function,
-	toggleSidebarExpanded?: Function,
+	toggleSidebarVisibility: Function,
+	toggleSidebarExpanded: Function,
+	forceCloseSidebar: Function,
 	isSidebarVisible?: boolean,
 	isSidebarExpanded?: boolean
 }
-type State = {
-	isSidebarVisible: boolean,
-	isSidebarExpanded: boolean
-}
 
-export default class View extends Component<Props, State> {
-	static defaultProps = {
-		isSidebarVisible: false,
-		isSidebarExpanded: true
-	}
+const View = (props: Props) => {
+	const {
+		sidebarItems,
+		user,
+		business,
+		getSearchSuggestionValue,
+		renderSearchSuggestion,
+		isSidebarVisible,
+		isSidebarExpanded,
+		toggleSidebarExpanded,
+		toggleSidebarVisibility,
+		forceCloseSidebar,
+		children
+	} = props
 
-	state = {
-		isSidebarVisible: this.props.isSidebarVisible,
-		isSidebarExpanded: this.props.isSidebarExpanded
-	}
-
-	toggleSidebarVisibility = () => {
-		this.setState(prevState => ({
-			isSidebarVisible: !prevState.isSidebarVisible,
-			isSidebarExpanded: true
-		}))
-		this.props.toggleSidebarVisibility && this.props.toggleSidebarVisibility()
-	}
-
-	toggleSidebarExpanded = () => {
-		this.setState(prevState => ({
-			isSidebarExpanded: !prevState.isSidebarExpanded
-		}))
-		this.props.toggleSidebarExpanded && this.props.toggleSidebarExpanded()
-	}
-
-	forceCloseSidebar = () => {
-		this.setState({
-			isSidebarVisible: false
-		})
-	}
-
-	render() {
-		const { isSidebarVisible, isSidebarExpanded } = this.state
-		const {
-			sidebarItems,
-			user,
-			business,
-			getSearchSuggestionValue,
-			renderSearchSuggestion,
-			children
-		} = this.props
-
-		return (
-			<div
-				className={cx('main-wrapper', {
-					'menu--is-visible': isSidebarVisible,
-					'sidebar--is-collapsed': !isSidebarExpanded,
-					'sidebar--is-missing': !sidebarItems || sidebarItems.length === 0
-				})}
-			>
-				{sidebarItems && sidebarItems.length > 0 && (
-					<Sidebar
-						items={sidebarItems}
-						isSidebarVisible={isSidebarVisible}
-						isExpanded={isSidebarExpanded}
-						toggleExpanded={this.toggleSidebarExpanded}
-						forceCloseSidebar={this.forceCloseSidebar}
-					/>
-				)}
-				<HeaderPrimary
-					user={user}
-					business={business}
-					enableHamburgerMenu={
-						sidebarItems && sidebarItems.length > 0 ? true : false
-					}
-					toggleSidebarVisibility={this.toggleSidebarVisibility}
+	return (
+		<div
+			className={cx('main-wrapper', {
+				'menu--is-visible': isSidebarVisible,
+				'sidebar--is-collapsed': !isSidebarExpanded,
+				'sidebar--is-missing': !sidebarItems || sidebarItems.length === 0
+			})}
+		>
+			{sidebarItems && sidebarItems.length > 0 && (
+				<Sidebar
+					items={sidebarItems}
 					isSidebarVisible={isSidebarVisible}
-					getSearchSuggestionValue={getSearchSuggestionValue}
-					renderSearchSuggestion={renderSearchSuggestion}
+					isExpanded={isSidebarExpanded}
+					toggleExpanded={toggleSidebarExpanded}
+					forceCloseSidebar={forceCloseSidebar}
 				/>
-				<main className="main-content">{children}</main>
-			</div>
-		)
-	}
+			)}
+			<HeaderPrimary
+				user={user}
+				business={business}
+				enableHamburgerMenu={
+					sidebarItems && sidebarItems.length > 0 ? true : false
+				}
+				toggleSidebarVisibility={toggleSidebarVisibility}
+				isSidebarVisible={isSidebarVisible}
+				getSearchSuggestionValue={getSearchSuggestionValue}
+				renderSearchSuggestion={renderSearchSuggestion}
+			/>
+			<main className="main-content">{children}</main>
+		</div>
+	)
 }
+
+export default View
