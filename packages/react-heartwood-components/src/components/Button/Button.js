340c6df3f8af7e7593c3dd5e3af44d3a79c2df5b
taylorromero <me>
2018-12-28 12:22:52 -0700

    button event

--- packages/react-heartwood-components/src/components/Button/Button.js
+++ packages/react-heartwood-components/src/components/Button/Button.js
@@ -85,7 +85,7 @@ const Button = (props: Props) => {
 	const handleClick = (e: any) => {
 		e.currentTarget.blur()
 		if (onClick) {
-			onClick(props.payload || e)
+			onClick(e, props.payload)
 		}
 	}
 
