02e1cd8827977f2163adc0073d35d4a81106cf05
Ken Goldfarb <hello@kengoldfarb.com>
2019-01-03 18:28:23 -0700

    cleanup

--- packages/spruce-skill/interface/pages/skill-views/location_dashboard/index.js
+++ packages/spruce-skill/interface/pages/skill-views/location_dashboard/index.js
@@ -17,17 +17,12 @@ class DashboardLocationPage extends React.Component {
 		log.debug('onDrop!')
 		log.debug({ acceptedFiles, rejectedFiles })
 		const req = request.put(`/api/1.0/upload.json`)
-		// .field('imageSizes', '320, 320@2x, 414, 414@2x, 700, 700@2x')
-		// .field('refId', refId)
-		// .field('organizationId', organizationId)
-		// .field('locationId', locationId)
-		// .field('teammateId', teammateId)
-		// .field('guestId', guestId)
+		// .field('imageSizes', '320, 320@2x, 414, 414@2x, 700, 700@2x') // Additional information can be sent by using .field(key, val)
 		acceptedFiles.forEach(file => {
-			req.attach('files', file)
+			req.attach('files', file) // Attach each file that was uploaded to the request
 		})
 		const res = await req
-		console.log({ ...res.body })
+		log.debug({ ...res.body })
 	}
 
 	render() {
@@ -40,6 +35,7 @@ class DashboardLocationPage extends React.Component {
 				/>
 				<PageContent>
 					<p>Welcome to the location dashboard example skill view!</p>
+					<p>{"Here's an example of uploading files"}</p>
 					<Dropzone onDrop={this.onDrop} />
 				</PageContent>
 			</Page>
