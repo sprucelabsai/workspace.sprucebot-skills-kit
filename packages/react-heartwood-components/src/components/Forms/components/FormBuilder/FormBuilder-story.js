0255659743e5c7dd844e7af42c900e6aa5cb810b
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-01-23 15:58:35 -0700

    minor: add sections for form as page

--- packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder-story.js
+++ packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder-story.js
@@ -31,231 +31,283 @@ const withLayout = storyFn => (
 )
 
 stories.addDecorator(withKnobs)
-stories.addDecorator(withLayout)
+// stories.addDecorator(withLayout)
 
 stories
 	.add('Basic', () => (
-		<FormBuilder
-			rows={[
-				{
-					id: 'settings',
-					fields: [
-						{
-							id: 'settingsList',
-							element: 'list',
-							items: [
-								{
-									title: 'Guest SMS cancellations',
-									subtitle: 'Guests can cancel an appointment via SMS',
-									toggleId: 'smsCancellations',
-									toggleProps: {
-										onChange: () => {
-											console.log('Boop')
+		<Page>
+			<PageContent>
+				<Layout>
+					<LayoutSection>
+						<Card>
+							<CardBody>
+								<FormBuilder
+									rows={[
+										{
+											id: 'settings',
+											fields: [
+												{
+													id: 'settingsList',
+													element: 'list',
+													items: [
+														{
+															title: 'Guest SMS cancellations',
+															subtitle:
+																'Guests can cancel an appointment via SMS',
+															toggleId: 'smsCancellations',
+															toggleProps: {
+																onChange: () => {
+																	console.log('Boop')
+																}
+															}
+														},
+														{
+															title: 'Guest SMS confirmations',
+															subtitle:
+																'Guests can confirm an appointment via SMS',
+															toggleId: 'smsConfirmations'
+														}
+													]
+												}
+											]
 										}
-									}
-								},
-								{
-									title: 'Guest SMS confirmations',
-									subtitle: 'Guests can confirm an appointment via SMS',
-									toggleId: 'smsConfirmations'
-								}
-							]
-						}
-					]
-				}
-			]}
-		/>
+									]}
+								/>
+							</CardBody>
+						</Card>
+					</LayoutSection>
+				</Layout>
+			</PageContent>
+		</Page>
 	))
 	.add('With Submit', () => (
-		<FormBuilder
-			validate={values => {
-				console.log('Validate: ', { values })
-				let errors = {}
-				if (!values.publicName) {
-					errors.publicName = 'Please include a public name'
-				}
-				if (!values.streetAddress) {
-					errors.streetAddress = 'Please include a street address'
-				}
-				if (!values.city) {
-					errors.city = 'Please include a city'
-				}
-				if (!values.postalCode) {
-					errors.postalCode = 'Please include a postal/zip code'
-				}
-				if (!values.country) {
-					errors.country = 'Please include a country'
-				}
-				if (!values.region) {
-					errors.region = 'Please include a state or region'
-				}
-				return errors
-			}}
-			onSubmit={values => console.log('Submit', { values })}
-			initialValues={{
-				publicName: 'Chimera Hair Salon',
-				streetAddress: '',
-				streetAddress2: '',
-				city: '',
-				postalCode: '',
-				country: 'United States',
-				region: ''
-			}}
-			formLayout={{
-				spacing: 'tight'
-			}}
-			rows={[
-				{
-					id: 'publicNameRow',
-					fields: [
-						{
-							element: 'textInput',
-							id: 'publicName',
-							label: 'Public Name',
-							helper: 'This is the name that your guests will see',
-							required: true
-						}
-					]
-				},
-				{
-					id: 'streetAddressRow',
-					fields: [
-						{
-							element: 'textInput',
-							id: 'streetAddress',
-							label: 'Street Address',
-							placeholder: '123 High St',
-							required: true
-						}
-					]
-				},
-				{
-					id: 'streetAddress2row',
-					fields: [
-						{
-							element: 'textInput',
-							id: 'streetAddress2',
-							label: 'Unit, Suite, Etc… (Optional)',
-							required: false
-						}
-					]
-				},
-				{
-					id: 'cityAndPostalRow',
-					fields: [
-						{
-							id: 'city',
-							element: 'textInput',
-							label: 'City',
-							placeholder: 'Springfield',
-							required: true
-						},
-						{
-							id: 'postalCode',
-							element: 'textInput',
-							label: 'Postal / Zip Code',
-							placeholder: '888888',
-							required: true
-						}
-					]
-				},
-				{
-					id: 'countryAndRegionRow',
-					fields: [
-						{
-							id: 'country',
-							element: 'textInput',
-							label: 'Country',
-							placeholder: 'Select an option…',
-							required: true
-						},
-						{
-							id: 'region',
-							element: 'textInput',
-							label: 'State',
-							placeholder: 'Select an option…',
-							required: true
-						}
-					]
-				}
-			]}
-			primaryCTA={{
-				text: 'Add location'
-			}}
-		/>
+		<Page>
+			<PageContent>
+				<Layout>
+					<LayoutSection>
+						<Card>
+							<CardBody>
+								<FormBuilder
+									validate={values => {
+										console.log('Validate: ', { values })
+										let errors = {}
+										if (!values.publicName) {
+											errors.publicName = 'Please include a public name'
+										}
+										if (!values.streetAddress) {
+											errors.streetAddress = 'Please include a street address'
+										}
+										if (!values.city) {
+											errors.city = 'Please include a city'
+										}
+										if (!values.postalCode) {
+											errors.postalCode = 'Please include a postal/zip code'
+										}
+										if (!values.country) {
+											errors.country = 'Please include a country'
+										}
+										if (!values.region) {
+											errors.region = 'Please include a state or region'
+										}
+										return errors
+									}}
+									onSubmit={values => console.log('Submit', { values })}
+									initialValues={{
+										publicName: 'Chimera Hair Salon',
+										streetAddress: '',
+										streetAddress2: '',
+										city: '',
+										postalCode: '',
+										country: 'United States',
+										region: ''
+									}}
+									formLayout={{
+										spacing: 'tight'
+									}}
+									rows={[
+										{
+											id: 'publicNameRow',
+											fields: [
+												{
+													element: 'textInput',
+													id: 'publicName',
+													label: 'Public Name',
+													helper: 'This is the name that your guests will see',
+													required: true
+												}
+											]
+										},
+										{
+											id: 'streetAddressRow',
+											fields: [
+												{
+													element: 'textInput',
+													id: 'streetAddress',
+													label: 'Street Address',
+													placeholder: '123 High St',
+													required: true
+												}
+											]
+										},
+										{
+											id: 'streetAddress2row',
+											fields: [
+												{
+													element: 'textInput',
+													id: 'streetAddress2',
+													label: 'Unit, Suite, Etc… (Optional)',
+													required: false
+												}
+											]
+										},
+										{
+											id: 'cityAndPostalRow',
+											fields: [
+												{
+													id: 'city',
+													element: 'textInput',
+													label: 'City',
+													placeholder: 'Springfield',
+													required: true
+												},
+												{
+													id: 'postalCode',
+													element: 'textInput',
+													label: 'Postal / Zip Code',
+													placeholder: '888888',
+													required: true
+												}
+											]
+										},
+										{
+											id: 'countryAndRegionRow',
+											fields: [
+												{
+													id: 'country',
+													element: 'textInput',
+													label: 'Country',
+													placeholder: 'Select an option…',
+													required: true
+												},
+												{
+													id: 'region',
+													element: 'textInput',
+													label: 'State',
+													placeholder: 'Select an option…',
+													required: true
+												}
+											]
+										}
+									]}
+									primaryCTA={{
+										text: 'Add location'
+									}}
+								/>
+							</CardBody>
+						</Card>
+					</LayoutSection>
+				</Layout>
+			</PageContent>
+		</Page>
 	))
 	.add('Settings page', () => (
-		<FormBuilder
-			initialValues={{
-				pastApptEditingCutoff: '45min',
-				pastApptBookingCutoff: '10min',
-				guestUpdateDelays: '1min',
-				autoConfirmAppts: true
-			}}
-			onSubmit={values => console.log('Submit', { values })}
-			formLayout={{
-				spacing: 'tight'
-			}}
-			rows={[
-				{
-					id: 'pastEditingCutoffRow',
-					fields: [
+		<Page>
+			<PageContent>
+				<FormBuilder
+					initialValues={{
+						pastApptEditingCutoff: '45min',
+						pastApptBookingCutoff: '10min',
+						guestUpdateDelays: '1min',
+						autoConfirmAppts: true
+					}}
+					onSubmit={values => console.log('Submit', { values })}
+					formLayout={{
+						spacing: 'tight'
+					}}
+					sections={[
 						{
-							element: 'textInput',
-							id: 'pastApptEditingCutoff',
-							title: 'Cutoff for editing appointments in the past',
-							description:
-								'Once an appointment is past, how many minutes should I wait until I block the ability to edit it? Note: Paid appointments cannot be edited.	'
-						}
-					]
-				},
-				{
-					id: 'pastApptBookingCutoffRow',
-					fields: [
-						{
-							element: 'textInput',
-							id: 'pastApptBookingCutoff',
-							title: 'Booking past appointment cutoff',
-							description:
-								'How far in the past should I allow appointments to be booked?	'
-						}
-					]
-				},
-				{
-					id: 'guestUpdateDelaysRow',
-					fields: [
-						{
-							element: 'textInput',
-							id: 'guestUpdateDelays',
-							title: 'Guest update delays',
-							description:
-								'How long should I wait before sending an update text to the guest when changes are made to their appointment? This gives you time to make a few edits back-to-back without the guest receiving a ton of texts.'
-						}
-					]
-				},
-				{
-					id: 'autoConfirmRow',
-					fields: [
-						{
-							id: 'autoConfirm',
-							element: 'list',
-							items: [
+							id: 'secOne',
+							rows: [
 								{
-									id: 'autoConfirm',
-									title: 'Auto-Confirm Appointments',
-									subtitle: 'Should I auto-confirm all booked appointments?',
-									toggleId: 'autoConfirm',
-									toggleProps: {
-										defaultChecked: true,
-										onChange: () => {
-											console.log('Boop')
+									id: 'pastEditingCutoffRow',
+									fields: [
+										{
+											element: 'textInput',
+											id: 'pastApptEditingCutoff',
+											title: 'Cutoff for editing appointments in the past',
+											description:
+												'Once an appointment is past, how many minutes should I wait until I block the ability to edit it? Note: Paid appointments cannot be edited.	'
 										}
-									}
+									]
+								},
+								{
+									id: 'pastApptBookingCutoffRow',
+									fields: [
+										{
+											element: 'textInput',
+											id: 'pastApptBookingCutoff',
+											title: 'Booking past appointment cutoff',
+											description:
+												'How far in the past should I allow appointments to be booked?	'
+										}
+									]
+								},
+								{
+									id: 'guestUpdateDelaysRow',
+									fields: [
+										{
+											element: 'textInput',
+											id: 'guestUpdateDelays',
+											title: 'Guest update delays',
+											description:
+												'How long should I wait before sending an update text to the guest when changes are made to their appointment? This gives you time to make a few edits back-to-back without the guest receiving a ton of texts.'
+										}
+									]
+								},
+								{
+									id: 'autoConfirmRow',
+									fields: [
+										{
+											id: 'autoConfirm',
+											element: 'list',
+											items: [
+												{
+													id: 'autoConfirm',
+													title: 'Auto-Confirm Appointments',
+													subtitle:
+														'Should I auto-confirm all booked appointments?',
+													toggleId: 'autoConfirm',
+													toggleProps: {
+														defaultChecked: true,
+														onChange: () => {
+															console.log('Boop')
+														}
+													}
+												}
+											]
+										}
+									]
+								}
+							]
+						},
+						{
+							id: 'section2',
+							rows: [
+								{
+									id: 'row1',
+									fields: [
+										{
+											id: 'field1',
+											element: 'textInput',
+											label: 'Name',
+											placeholder: 'e.g. Ralph'
+										}
+									]
 								}
 							]
 						}
-					]
-				}
-			]}
-		/>
+					]}
+				/>
+			</PageContent>
+		</Page>
 	))
--- packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder.js
+++ packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder.js
@@ -1,13 +1,23 @@
 // @flow
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Formik, Form } from 'formik'
 import FormInner from './components/FormInner/FormInner'
+import Card, { CardBody } from '../../../Card'
+import Layout, { LayoutSection } from '../../../Layout'
 
-import type { FormInnerProps } from './components/FormInner/FormInner'
+import type { FormInnerRowProps } from './components/FormInner/FormInner'
+import type { FormLayoutProps } from '../FormLayout/FormLayout'
+import type { Props as ButtonProps } from '../../../Button/Button'
+
+type Section = {
+	id: string,
+	rows: Array<FormInnerRowProps>
+}
 
 type Props = {
 	initialValues: Object,
 	onSubmit: Function,
+	sections: Array<Section>,
 	rows: Array<FormInnerRowProps>,
 	formLayout: FormLayoutProps,
 	validate?: Function,
@@ -19,6 +29,7 @@ const FormBuilder = (props: Props) => {
 	const {
 		initialValues,
 		onSubmit,
+		sections,
 		rows,
 		formLayout,
 		primaryCTA,
@@ -35,6 +46,23 @@ const FormBuilder = (props: Props) => {
 				const { values, errors, touched, handleChange, handleBlur } = props
 				return (
 					<Form className="formbuilder">
+						{sections && sections.length > 0 && (
+							<Layout>
+								<LayoutSection>
+									{sections.map(section => (
+										<Card key={section.id}>
+											<CardBody>
+												<FormInner
+													formLayout={formLayout}
+													rows={section.rows}
+													formikProps={props}
+												/>
+											</CardBody>
+										</Card>
+									))}
+								</LayoutSection>
+							</Layout>
+						)}
 						<FormInner
 							formLayout={formLayout}
 							rows={rows}
