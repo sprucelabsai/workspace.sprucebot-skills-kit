0beeec351353d91539e8fe282c91e471cb0b80e6
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-01-23 09:26:21 -0700

    minor: add example with validation

--- packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder-story.js
+++ packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder-story.js
@@ -11,7 +11,7 @@ import {
 } from '@storybook/addon-knobs/react'
 import FormBuilder from './FormBuilder'
 import Page, { PageContent } from '../../../Page'
-import Card from '../../../Card'
+import Card, { CardBody, CardSection } from '../../../Card'
 import Layout, { LayoutSection } from '../../../Layout'
 
 const stories = storiesOf('FormBuilder', module)
@@ -21,7 +21,9 @@ const withLayout = storyFn => (
 		<PageContent>
 			<Layout>
 				<LayoutSection>
-					<Card>{storyFn()}</Card>
+					<Card>
+						<CardBody>{storyFn()}</CardBody>
+					</Card>
 				</LayoutSection>
 			</Layout>
 		</PageContent>
@@ -31,35 +33,152 @@ const withLayout = storyFn => (
 stories.addDecorator(withKnobs)
 stories.addDecorator(withLayout)
 
-stories.add('Basic', () => (
-	<FormBuilder
-		rows={[
-			{
-				id: 'settings',
-				fields: [
-					{
-						id: 'settingsList',
-						element: 'list',
-						items: [
-							{
-								title: 'Guest SMS cancellations',
-								subtitle: 'Guests can cancel an appointment via SMS',
-								toggleId: 'smsCancellations',
-								toggleProps: {
-									onChange: () => {
-										console.log('Boop')
+stories
+	.add('Basic', () => (
+		<FormBuilder
+			rows={[
+				{
+					id: 'settings',
+					fields: [
+						{
+							id: 'settingsList',
+							element: 'list',
+							items: [
+								{
+									title: 'Guest SMS cancellations',
+									subtitle: 'Guests can cancel an appointment via SMS',
+									toggleId: 'smsCancellations',
+									toggleProps: {
+										onChange: () => {
+											console.log('Boop')
+										}
 									}
+								},
+								{
+									title: 'Guest SMS confirmations',
+									subtitle: 'Guests can confirm an appointment via SMS',
+									toggleId: 'smsConfirmations'
 								}
-							},
-							{
-								title: 'Guest SMS confirmations',
-								subtitle: 'Guests can confirm an appointment via SMS',
-								toggleId: 'smsConfirmations'
-							}
-						]
-					}
-				]
-			}
-		]}
-	/>
-))
+							]
+						}
+					]
+				}
+			]}
+		/>
+	))
+	.add('With Submit', () => (
+		<FormBuilder
+			validate={values => {
+				let errors = {}
+				if (!values.publicName) {
+					errors.publicName = 'Please include a public name'
+				}
+				if (!values.streetAddress) {
+					errors.streetAddress = 'Please include a street address'
+				}
+				if (!values.city) {
+					errors.city = 'Please include a city'
+				}
+				if (!values.postalCode) {
+					errors.postalCode = 'Please include a postal/zip code'
+				}
+				if (!values.country) {
+					errors.country = 'Please include a country'
+				}
+				if (!values.region) {
+					errors.region = 'Please include a state or region'
+				}
+				return errors
+			}}
+			initialValues={{
+				publicName: 'Chimera Hair Salon',
+				streetAddress: '',
+				streetAddress2: '',
+				city: '',
+				postalCode: '',
+				country: 'United States',
+				region: ''
+			}}
+			formLayout={{
+				spacing: 'tight'
+			}}
+			rows={[
+				{
+					id: 'publicNameRow',
+					fields: [
+						{
+							element: 'textInput',
+							id: 'publicName',
+							label: 'Public Name',
+							helper: 'This is the name that your guests will see',
+							required: true
+						}
+					]
+				},
+				{
+					id: 'streetAddressRow',
+					fields: [
+						{
+							element: 'textInput',
+							id: 'streetAddress',
+							label: 'Street Address',
+							placeholder: '123 High St',
+							required: true
+						}
+					]
+				},
+				{
+					id: 'streetAddress2row',
+					fields: [
+						{
+							element: 'textInput',
+							id: 'streetAddress2',
+							label: 'Unit, Suite, Etc… (Optional)',
+							required: false
+						}
+					]
+				},
+				{
+					id: 'cityAndPostalRow',
+					fields: [
+						{
+							id: 'city',
+							element: 'textInput',
+							label: 'City',
+							placeholder: 'Springfield',
+							required: true
+						},
+						{
+							id: 'postalCode',
+							element: 'textInput',
+							label: 'Postal / Zip Code',
+							placeholder: '888888',
+							required: true
+						}
+					]
+				},
+				{
+					id: 'countryAndRegionRow',
+					fields: [
+						{
+							id: 'country',
+							element: 'textInput',
+							label: 'Country',
+							placeholder: 'Select an option…',
+							required: true
+						},
+						{
+							id: 'region',
+							element: 'textInput',
+							label: 'State',
+							placeholder: 'Select an option…',
+							required: true
+						}
+					]
+				}
+			]}
+			primaryCTA={{
+				text: 'Add location'
+			}}
+		/>
+	))
--- packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder.js
+++ packages/react-heartwood-components/src/components/Forms/components/FormBuilder/FormBuilder.js
@@ -8,10 +8,12 @@ import {
 	FormLayoutItem
 } from '../../index'
 import List from '../../../List'
+import Button from '../../../Button/Button'
 
 import type { FormLayoutProps } from '../FormLayout'
 import type { FormLayoutGroupProps } from '../FormLayout/components/FormLayoutGroup'
 import type { FormLayoutItemProps } from '../FormLayout/components/FormLayoutItem'
+import type { Props as ButtonProps } from '../../../Button/Button'
 
 type Field = {
 	id: string,
@@ -29,11 +31,22 @@ type Props = {
 	initialValues: Object,
 	onSubmit: Function,
 	rows: Array<Row>,
-	formLayout: FormLayoutProps
+	formLayout: FormLayoutProps,
+	validate?: Function,
+	primaryCTA?: ButtonProps,
+	secondaryCTA?: ButtonProps
 }
 
 const FormBuilder = (props: Props) => {
-	const { initialValues, onSubmit, rows, formLayout } = props
+	const {
+		initialValues,
+		onSubmit,
+		rows,
+		formLayout,
+		primaryCTA,
+		secondaryCTA,
+		validate
+	} = props
 
 	const Elements = {
 		textInput: TextInput,
@@ -42,38 +55,63 @@ const FormBuilder = (props: Props) => {
 	return (
 		<Formik
 			initialValues={initialValues}
+			validate={values => validate(values)}
 			onSubmit={onSubmit}
-			render={props => (
-				<Form>
-					<FormLayout {...formLayout}>
-						{rows &&
-							rows.length > 0 &&
-							rows.map(row => (
-								<FormLayoutGroup key={row.id}>
-									{row.fields &&
-										row.fields.length > 0 &&
-										row.fields.map(field => {
-											const { element, ...rest } = field
-											const Handler = Elements[field.element]
-											return (
-												<FormLayoutItem key={field.id}>
-													{Handler && (
-														<Handler
-															onChange={props.handleChange}
-															onBlur={props.handleBlur}
-															{...rest}
-														/>
-													)}
-												</FormLayoutItem>
-											)
-										})}
+			render={props => {
+				const { values, errors, touched, handleChange, handleBlur } = props
+				console.log({ props })
+				return (
+					<Form>
+						<FormLayout {...formLayout}>
+							{rows &&
+								rows.length > 0 &&
+								rows.map(row => (
+									<FormLayoutGroup key={row.id}>
+										{row.fields &&
+											row.fields.length > 0 &&
+											row.fields.map(field => {
+												const { element, ...rest } = field
+												const Handler = Elements[field.element]
+												return (
+													<FormLayoutItem key={field.id}>
+														{Handler && (
+															<Handler
+																name={field.id}
+																onChange={handleChange}
+																onBlur={handleBlur}
+																value={values[field.id]}
+																error={touched[field.id] && errors[field.id]}
+																{...rest}
+															/>
+														)}
+													</FormLayoutItem>
+												)
+											})}
+									</FormLayoutGroup>
+								))}
+							{primaryCTA && (
+								<FormLayoutGroup>
+									{secondaryCTA && <Button {...secondaryCTA} />}
+									<Button
+										kind="primary"
+										type="submit"
+										disabled={!props.isValid}
+										{...primaryCTA}
+									/>
 								</FormLayoutGroup>
-							))}
-					</FormLayout>
-				</Form>
-			)}
+							)}
+						</FormLayout>
+					</Form>
+				)
+			}}
 		/>
 	)
+}
+
+FormBuilder.defaultProps = {
+	validate: () => null,
+	primaryCTA: null,
+	secondaryCTA: null
 }
 
 export default FormBuilder
