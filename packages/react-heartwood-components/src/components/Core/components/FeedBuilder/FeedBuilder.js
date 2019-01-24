fd987f5bf235a9a1013340163411f282fd2b2f54
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-01-24 10:19:28 -0700

    minor: add props

--- packages/react-heartwood-components/src/components/Core/components/FeedBuilder/FeedBuilder.js
+++ packages/react-heartwood-components/src/components/Core/components/FeedBuilder/FeedBuilder.js
@@ -1,10 +1,23 @@
 // @flow
 import React from 'react'
+import Message, { MessageBuilder } from '../../../Message'
 
-type Props = {}
+import type { MessageBuilderProps } from '../../../Message'
+
+type MessageProps = {
+	/** Unique id for the message */
+	id: string,
+	...MessageBuilderProps
+}
+
+type Props = {
+	/** Messages for the feed */
+	messages: Array<MessageProps>
+}
 
 const FeedBuilder = (props: Props) => {
-	return <p>Feedbulder</p>
+	const { messages } = props
+	return <div className="message-feed" />
 }
 
 FeedBuilder.defaultProps = {}
