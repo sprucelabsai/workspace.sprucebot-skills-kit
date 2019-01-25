f5cdce1132b69d6caa052eb816a4de31a550f6e5
Brandon Minch <brandon@barbershop.io>
2019-01-24 18:53:41 -0700

    search progress

--- packages/react-heartwood-components/src/components/BigSearch/BigSearch-story.js
+++ packages/react-heartwood-components/src/components/BigSearch/BigSearch-story.js
@@ -6,7 +6,79 @@ import BigSearch from '../BigSearch/BigSearch'
 
 const stories = storiesOf('Big Search', module)
 
+import user01image from '../../../static/assets/users/user-01--96w.png'
+import user02image from '../../../static/assets/users/user-02--96w.png'
+import user03image from '../../../static/assets/users/user-03--96w.png'
+import user04image from '../../../static/assets/users/user-04--96w.png'
+import user05image from '../../../static/assets/users/user-05--96w.png'
+import user06image from '../../../static/assets/users/user-06--96w.png'
+
+const recentSearchResults = [
+	{
+		header: {
+			title: 'Recent Searches'
+		},
+		items: [
+			{
+				avatar: user01image,
+				title: 'Lacey Morissette',
+				subtitle: 'Guest',
+				actions: []
+			},
+			{
+				avatar: user02image,
+				title: 'Jade Mohr',
+				subtitle: 'Stylist',
+				actions: []
+			},
+			{
+				avatar: user03image,
+				title: 'Gianni Block',
+				subtitle: 'Guest',
+				actions: []
+			}
+		]
+	}
+]
+
+const suggestedSearchResults = [
+	{
+		items: [
+			{
+				avatar: user04image,
+				title: 'Vicenta Maggio',
+				subtitle: 'Guest',
+				actions: []
+			},
+			{
+				avatar: user05image,
+				title: 'Vicenta Ramirez',
+				subtitle: 'Guest',
+				actions: []
+			},
+			{
+				avatar: user06image,
+				title: 'Allison Vicenta',
+				subtitle: 'Manager',
+				actions: []
+			}
+		]
+	}
+]
+
 stories.addDecorator(withKnobs)
-stories.add('Big Search', () => {
-	return <BigSearch />
-})
+stories
+	.add('Default', () => {
+		return (
+			<BigSearch isVisible={true} initialSearchResults={recentSearchResults} />
+		)
+	})
+	.add('Suggested Search Results', () => {
+		return (
+			<BigSearch
+				isVisible={true}
+				initialSearchResults={recentSearchResults}
+				suggestedSearchResults={suggestedSearchResults}
+			/>
+		)
+	})
--- packages/react-heartwood-components/src/components/BigSearch/BigSearch.js
+++ packages/react-heartwood-components/src/components/BigSearch/BigSearch.js
@@ -1,44 +1,131 @@
 // @flow
 import React, { Component } from 'react'
 import { Formik, Form } from 'formik'
+import cx from 'classnames'
+import { CSSTransition } from 'react-transition-group'
 
+import Icon from '../Icon/Icon'
+import List from '../List/List'
+import { InputInner } from '../Forms/FormPartials'
 import Button from '../Button/Button'
-import { Search } from '../Forms'
 
-type Props = {}
+import type { Props as ListProps } from '../List/List'
+
+type Props = {
+	/** The list of initial results to show when search is displayed */
+	initialSearchResults?: Array<ListProps>,
+
+	/** The search results to display as the user is entering search query */
+	suggestedSearchResults?: Array<ListProps>,
+
+	/** Whether search is visible */
+	isVisible: boolean
+}
 
 type State = {
-	isAddGuestShowing: boolean
+	isAddGuestShowing: boolean,
+	searchValue: string,
+	searchContextBarIsHighlighted: boolean
 }
 
 export default class BigSearch extends Component<Props, State> {
-	static defaultProps = {}
+	static defaultProps = {
+		isVisible: false
+	}
 
 	state = {
-		isAddGuestShowing: false
+		isAddGuestShowing: false,
+		searchValue: '',
+		searchContextBarIsHighlighted: false
+	}
+
+	renderSearchResults = (results: ListProps) => {
+		return <List isSmall={true} {...results} />
+	}
+
+	handleSearchValueChange = (e: any) => {
+		const searchValue = e.target.value
+		this.setState({
+			searchValue,
+			searchContextBarIsHighlighted: searchValue !== ''
+		})
+	}
+
+	handleClearSearchValue = () => {
+		this.setState({ searchValue: '' })
 	}
 
 	render() {
-		const { isAddGuestShowing } = this.state
+		const {
+			isAddGuestShowing,
+			searchValue,
+			searchContextBarIsHighlighted
+		} = this.state
+
+		const {
+			initialSearchResults,
+			suggestedSearchResults,
+			isVisible
+		} = this.props
+
 		return (
-			<div class="big-search-overlay">
-				<div class="big-search">
-					<div class="big-search__search-view-header">
-						<div class="big-search__search-view-search-bar">
-							<Search type="text" placeholder={'CHANGE ME'} readOnly={false} />
-							<div class="big-search__search-view-search-bar-buttons">
-								<Button text="Clear" />
-								<Button icon={{ name: 'close' }} />
+			<CSSTransition
+				in={isVisible}
+				appear={true}
+				classNames="big-search-overlay"
+				timeout={100}
+			>
+				<div
+					className={cx('big-search-overlay', {
+						'big-search-overlay--visible': isVisible
+					})}
+				>
+					<div className="big-search">
+						<div className="big-search__search-view-header">
+							<div className="big-search__search-view-search-bar">
+								<div className={'text-input'}>
+									<div class="text-input__inner">
+										<Icon icon={'search'} className="text-input__icon-pre" />
+										<input
+											class="text-input__input"
+											placeholder="CHANGE ME"
+											value={searchValue}
+											onChange={this.handleSearchValueChange}
+										/>
+									</div>
+								</div>
+								<div className="big-search__search-view-search-bar-buttons">
+									<Button text="Clear" onClick={this.handleClearSearchValue} />
+									<Button icon={{ name: 'close' }} />
+								</div>
 							</div>
+							{!searchValue ? (
+								<Button
+									className="big-search__search-view-add-btn"
+									text="Quick add a new guest"
+									icon={{ name: 'add' }}
+								/>
+							) : (
+								<Button
+									className={cx('big-search__search-view-context-bar-btn', {
+										'big-search__search-view-context-bar-btn--highlighted': searchContextBarIsHighlighted
+									})}
+									text={searchValue}
+									icon={{ name: 'search' }}
+								/>
+							)}
 						</div>
-						<Button
-							className="big-search__search-view-context-btn"
-							text="Quick add a new guest"
-							icon={{ name: 'add' }}
-						/>
+						<div className="big-search__view-body">
+							{searchValue === '' &&
+								initialSearchResults &&
+								initialSearchResults.map(this.renderSearchResults)}
+							{searchValue !== '' &&
+								suggestedSearchResults &&
+								suggestedSearchResults.map(this.renderSearchResults)}
+						</div>
 					</div>
 				</div>
-			</div>
+			</CSSTransition>
 		)
 	}
 }
