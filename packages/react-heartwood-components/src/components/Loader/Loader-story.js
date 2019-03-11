211064a82394d13dc70f3f957f87e98c3bf9273e
Brandon Minch <brandon@barbershop.io>
2019-03-11 17:06:07 -0600

    refactor(loader): add isLight prop [SBL-2236]

--- packages/react-heartwood-components/src/components/Loader/Loader-story.js
+++ packages/react-heartwood-components/src/components/Loader/Loader-story.js
@@ -1,5 +1,22 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withKnobs, boolean } from '@storybook/addon-knobs/react'
 import Loader from './Loader'
 
-storiesOf('Loader', module).add('Loader', () => <Loader />)
+const stories = storiesOf('Loader', module)
+
+stories.addDecorator(withKnobs)
+
+stories.add('Loader', () => {
+	const isLight = boolean('isLight', false)
+	return (
+		<div
+			style={{
+				backgroundColor: isLight ? 'black' : 'transparent',
+				padding: '2rem'
+			}}
+		>
+			<Loader isLight={isLight} />
+		</div>
+	)
+})
--- packages/react-heartwood-components/src/components/Loader/Loader.js
+++ packages/react-heartwood-components/src/components/Loader/Loader.js
@@ -1,12 +1,25 @@
 // @flow
 import React from 'react'
+import cx from 'classnames'
 
-const Loader = () => (
-	<span className="loader">
-		<span className="loader__dot" />
-		<span className="loader__dot" />
-		<span className="loader__dot" />
-	</span>
-)
+type Props = {
+	/* set to true if loader is on a dark background */
+	isLight: boolean
+}
+
+const Loader = (props: Props) => {
+	const { isLight } = props
+	return (
+		<span className={cx('loader', { 'loader--light': isLight })}>
+			<span className="loader__dot" />
+			<span className="loader__dot" />
+			<span className="loader__dot" />
+		</span>
+	)
+}
+
+Loader.defaultProps = {
+	isLight: false
+}
 
 export default Loader
