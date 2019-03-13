d6ddd40c847c2de3b538fcef552fe7a749899d92
Brandon Minch <brandon@barbershop.io>
2019-03-13 16:30:19 -0600

    refactor: remove underscore from private modal property

--- packages/spruce-skill/interface/pages/skill-views/example_skill_view_dialog.js
+++ packages/spruce-skill/interface/pages/skill-views/example_skill_view_dialog.js
@@ -17,7 +17,7 @@ type Props = {
 }
 
 class TestSkillView extends React.Component<Props> {
-	_modal = this.props.skill.modal()
+	modal = this.props.skill.modal()
 
 	static async getInitialProps(props: WrappedInitialProps) {
 		if (props.auth && props.auth.User) {
@@ -31,7 +31,7 @@ class TestSkillView extends React.Component<Props> {
 	async componentDidMount() {
 		this.props.skill.ready() // Show the skill
 
-		this._modal.onClickFooterPrimaryAction(() => {
+		this.modal.onClickFooterPrimaryAction(() => {
 			this.setModalProps(true)
 
 			setTimeout(() => {
@@ -39,13 +39,13 @@ class TestSkillView extends React.Component<Props> {
 			}, 1000)
 		})
 
-		this._modal.onClickFooterSecondaryAction(() => this._modal.close())
+		this.modal.onClickFooterSecondaryAction(() => this.modal.close())
 	}
 
 	setModalProps = (isSubmitting: boolean) => {
-		this._modal.setFooterSecondaryActionIsDisabled(isSubmitting)
-		this._modal.setFooterPrimaryActionIsDisabled(isSubmitting)
-		this._modal.setFooterPrimaryActionIsLoading(isSubmitting)
+		this.modal.setFooterSecondaryActionIsDisabled(isSubmitting)
+		this.modal.setFooterPrimaryActionIsDisabled(isSubmitting)
+		this.modal.setFooterPrimaryActionIsLoading(isSubmitting)
 	}
 
 	render() {
@@ -60,7 +60,7 @@ class TestSkillView extends React.Component<Props> {
 								kind="primary"
 								disabled={false}
 								icon={{ name: 'remove', isLineIcon: true }}
-								onClick={this._modal.close}
+								onClick={this.modal.close}
 							/>
 						</LayoutSection>
 					</Layout>
