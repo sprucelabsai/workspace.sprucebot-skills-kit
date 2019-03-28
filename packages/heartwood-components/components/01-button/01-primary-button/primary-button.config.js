79f95f1e06a408173fd00ea78952938a648be021
George Pantazis <gcpantazis@gmail.com>
2019-03-28 14:28:56 -0500

    feat(lint): Standardize lint for heartwood-components

--- packages/heartwood-components/components/01-button/01-primary-button/primary-button.config.js
+++ packages/heartwood-components/components/01-button/01-primary-button/primary-button.config.js
@@ -3,7 +3,11 @@ module.exports = {
 	status: 'wip',
 	collated: true,
 	collator: function(markup, item) {
-        return `<!-- Start: @${item.handle} -->\n<span style="display: inline-block; margin: 0 1rem 1rem 0;">${markup}</span>\n<!-- End: @${item.handle} -->\n`
+		return `<!-- Start: @${
+			item.handle
+		} -->\n<span style="display: inline-block; margin: 0 1rem 1rem 0;">${markup}</span>\n<!-- End: @${
+			item.handle
+		} -->\n`
 	},
 	context: {
 		text: 'Sign Up',
@@ -16,28 +20,32 @@ module.exports = {
 			context: {
 				disabled: true
 			}
-		}, {
+		},
+		{
 			name: 'loading',
 			context: {
-				className: "btn-primary btn--loading",
+				className: 'btn-primary btn--loading',
 				disabled: true,
 				loading: true
 			}
-		}, {
+		},
+		{
 			name: 'icon',
 			context: {
 				text: 'Add a Location',
-				icon: '<path d="M0 0h24v24H0z" fill="none"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>'
-
+				icon:
+					'<path d="M0 0h24v24H0z" fill="none"/><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>'
 			}
-		}, {
+		},
+		{
 			name: 'icon only',
 			context: {
 				text: null,
 				width: '20',
 				height: '20',
-				icon: '<path fill-rule="evenodd" clip-rule="evenodd" d="M10.8333 5.83203H9.16666V9.16536H5.83333V10.832H9.16666V14.1654H10.8333V10.832H14.1667V9.16536H10.8333V5.83203V5.83203ZM10 1.66602C5.4 1.66602 1.66666 5.39935 1.66666 9.99935C1.66666 14.5993 5.4 18.3327 10 18.3327C14.6 18.3327 18.3333 14.5993 18.3333 9.99935C18.3333 5.39935 14.6 1.66602 10 1.66602V1.66602ZM9.99999 16.6654C6.32499 16.6654 3.33333 13.6737 3.33333 9.9987C3.33333 6.3237 6.32499 3.33203 9.99999 3.33203C13.675 3.33203 16.6667 6.3237 16.6667 9.9987C16.6667 13.6737 13.675 16.6654 9.99999 16.6654V16.6654Z"/>'
+				icon:
+					'<path fill-rule="evenodd" clip-rule="evenodd" d="M10.8333 5.83203H9.16666V9.16536H5.83333V10.832H9.16666V14.1654H10.8333V10.832H14.1667V9.16536H10.8333V5.83203V5.83203ZM10 1.66602C5.4 1.66602 1.66666 5.39935 1.66666 9.99935C1.66666 14.5993 5.4 18.3327 10 18.3327C14.6 18.3327 18.3333 14.5993 18.3333 9.99935C18.3333 5.39935 14.6 1.66602 10 1.66602V1.66602ZM9.99999 16.6654C6.32499 16.6654 3.33333 13.6737 3.33333 9.9987C3.33333 6.3237 6.32499 3.33203 9.99999 3.33203C13.675 3.33203 16.6667 6.3237 16.6667 9.9987C16.6667 13.6737 13.675 16.6654 9.99999 16.6654V16.6654Z"/>'
 			}
 		}
 	]
-  }
+}
--- packages/heartwood-components/components/02-forms/05-phone-input/README.md
+++ packages/heartwood-components/components/02-forms/05-phone-input/README.md
@@ -1,9 +1,11 @@
 # Phone Input
+
 Phone Inputs accept phone numbers based on the selected country. This helps us increase the likelihood that the user will enter a valid phone number.
 
 ## When to use:
+
 Only use the Phone Input to ask for phone number.
 
 ## When not to use:
-- When not asking for a phone number; use another type of input
 
+- When not asking for a phone number; use another type of input
