26a59deab28d9878c71b6ecdbb4fa17ea10c7e7f
Ken Goldfarb <hello@kengoldfarb.com>
2019-06-12 14:22:27 -0600

    fix: event handler not getting called for v2 events

--- packages/spruce-skill-server/middleware/auth.js
+++ packages/spruce-skill-server/middleware/auth.js
@@ -147,6 +147,8 @@ module.exports = (router, options) => {
 							event: ctx.event
 						})
 					}
+
+					await listenersByEventName[eventName](ctx, next)
 				} catch (err) {
 					debug('(EVENT_VERSION=2) MIDDLEWARE/AUTH INVALID EVENT TOKEN', err)
 					if (config.LOG_EVENTS) {
