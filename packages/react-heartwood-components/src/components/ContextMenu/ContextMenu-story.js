531b972b10f926036a46eb6744b398cb9d9dbd32
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-06-14 14:59:46 -0600

    minor: update story

--- packages/react-heartwood-components/src/components/ContextMenu/ContextMenu-story.js
+++ packages/react-heartwood-components/src/components/ContextMenu/ContextMenu-story.js
@@ -8,92 +8,16 @@ const stories = storiesOf('Context Menu', module)
 stories.addDecorator(withKnobs)
 
 stories.add('Example', () => (
-	<div
-		style={{
-			position: 'relative',
-			width: '100vw',
-			height: '200vh'
-		}}
-	>
-		<div
-			style={{
-				position: 'absolute',
-				top: 0,
-				left: 0
-			}}
-		>
-			<ContextMenu
-				actions={object('actions', [
-					{ text: 'Rebook' },
-					{ text: 'Cancel appointment' },
-					{ text: 'Ask for feedback' }
-				])}
-				isRightAligned={boolean('isRightAligned', false)}
-				isBottomAligned={boolean('isBottomAligned', false)}
-				size={text('size', 'large')}
-				isSimple={boolean('isSimple', false)}
-				isSmall={boolean('isSmall', false)}
-			/>
-		</div>
-		<div
-			style={{
-				position: 'absolute',
-				top: 0,
-				right: 0
-			}}
-		>
-			<ContextMenu
-				actions={object('actions', [
-					{ text: 'Rebook' },
-					{ text: 'Cancel appointment' },
-					{ text: 'Ask for feedback' }
-				])}
-				isRightAligned={boolean('isRightAligned', false)}
-				isBottomAligned={boolean('isBottomAligned', false)}
-				size={text('size', 'large')}
-				isSimple={boolean('isSimple', false)}
-				isSmall={boolean('isSmall', false)}
-			/>
-		</div>
-		<div
-			style={{
-				position: 'absolute',
-				bottom: 0,
-				left: 0
-			}}
-		>
-			<ContextMenu
-				actions={object('actions', [
-					{ text: 'Rebook' },
-					{ text: 'Cancel appointment' },
-					{ text: 'Ask for feedback' }
-				])}
-				isRightAligned={boolean('isRightAligned', false)}
-				isBottomAligned={boolean('isBottomAligned', false)}
-				size={text('size', 'large')}
-				isSimple={boolean('isSimple', false)}
-				isSmall={boolean('isSmall', false)}
-			/>
-		</div>
-		<div
-			style={{
-				position: 'absolute',
-				bottom: 0,
-				right: 0
-			}}
-		>
-			<ContextMenu
-				actions={object('actions', [
-					{ text: 'Rebook' },
-					{ text: 'Cancel appointment' },
-					{ text: 'Ask for feedback' }
-				])}
-				isRightAligned={boolean('isRightAligned', false)}
-				isBottomAligned={boolean('isBottomAligned', false)}
-				size={text('size', 'large')}
-				isSimple={boolean('isSimple', false)}
-				isSmall={boolean('isSmall', false)}
-			/>
-		</div>
-	</div>
+	<ContextMenu
+		actions={object('actions', [
+			{ text: 'Rebook' },
+			{ text: 'Cancel appointment' },
+			{ text: 'Ask for feedback' }
+		])}
+		isRightAligned={boolean('isRightAligned', false)}
+		isBottomAligned={boolean('isBottomAligned', false)}
+		size={text('size', 'large')}
+		isSimple={boolean('isSimple', false)}
+		isSmall={boolean('isSmall', false)}
+	/>
 ))
