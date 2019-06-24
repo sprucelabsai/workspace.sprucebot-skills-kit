2164d0c79fb19f044e2dcb9c5ba54fc6d8843118
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-06-24 16:24:12 -0600

    minor: add avatar pages

--- packages/react-heartwood-components/.docz/app/imports.js
+++ packages/react-heartwood-components/.docz/app/imports.js
@@ -3,13 +3,21 @@ export const imports = {
     import(
       /* webpackPrefetch: true, webpackChunkName: "docz-index" */ 'docz/index.mdx'
     ),
-  'src/components/Button/Button.mdx': () =>
+  'src/components/Avatar/Avatar.mdx': () =>
     import(
-      /* webpackPrefetch: true, webpackChunkName: "src-components-button-button" */ 'src/components/Button/Button.mdx'
+      /* webpackPrefetch: true, webpackChunkName: "src-components-avatar-avatar" */ 'src/components/Avatar/Avatar.mdx'
+    ),
+  'src/components/Avatar/UserAvatar.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-avatar-user-avatar" */ 'src/components/Avatar/UserAvatar.mdx'
     ),
   'src/components/ButtonGroup/ButtonGroup.mdx': () =>
     import(
       /* webpackPrefetch: true, webpackChunkName: "src-components-button-group-button-group" */ 'src/components/ButtonGroup/ButtonGroup.mdx'
+    ),
+  'src/components/Button/Button.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-components-button-button" */ 'src/components/Button/Button.mdx'
     ),
   'src/components/ContextMenu/ContextMenu.mdx': () =>
     import(
