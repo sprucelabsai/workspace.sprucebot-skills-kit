ffd3854db100e04420017d01b07cc3ff4df64ced
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-07-03 08:18:26 -0600

    minor: fill out split button

--- packages/react-heartwood-components/src/components/SplitButton/SplitButton-story.js
+++ packages/react-heartwood-components/src/components/SplitButton/SplitButton-story.js
@@ -3,6 +3,18 @@ import { storiesOf } from '@storybook/react'
 
 import SplitButton from './SplitButton'
 
-storiesOf('SplitButton', module).add('Split Button', () => (
-	<SplitButton defaultAction="Hello" />
-))
+storiesOf('SplitButton', module)
+	.add('Primary', () => (
+		<SplitButton
+			defaultAction={{ text: 'Do the default action' }}
+			actions={[{ text: 'Thing two' }]}
+			kind="primary"
+		/>
+	))
+	.add('Secondary', () => (
+		<SplitButton
+			defaultAction={{ text: 'Do the default action' }}
+			actions={[{ text: 'Thing two' }]}
+			kind="secondary"
+		/>
+	))
--- packages/react-heartwood-components/src/components/SplitButton/SplitButton.tsx
+++ packages/react-heartwood-components/src/components/SplitButton/SplitButton.tsx
@@ -1,7 +1,142 @@
 import React from 'react'
+import Button from '../Button/Button'
+import ButtonGroup from '../ButtonGroup/ButtonGroup'
 
-const SplitButton = (props: { defaultAction: string }): React.ReactNode => {
-	return <div className="split-button">{props.defaultAction}</div>
+interface IIconProps {
+	/** The name of the icon to render. If not found, this will return null. */
+	icon?: string
+
+	/** Set true to render an icon with a stroke, but no fill */
+	isLineIcon?: boolean
+
+	customIcon?: any
+
+	className?: string
+}
+
+interface IButtonProps {
+	/** Optional class to add to the button. */
+	className?: string
+
+	/** Optional children passed into button */
+	children?: Node
+
+	/** Sets the visual appearance of the button. May be primary, secondary, simple, or caution. */
+	kind?: string
+
+	/** Set true to make the button less tall. */
+	isSmall?: boolean
+
+	/** Set true to make the button fill its parent's width. */
+	isFullWidth?: boolean
+
+	/** Set true to hide any text or icon in the button and show a loader instead. */
+	isLoading?: boolean
+
+	/** Set true to hide any text in the button. Text should still be provided for accessibility. */
+	isIconOnly?: boolean
+
+	/** Text for the button. */
+	text?: string
+
+	/** Will render a link. May be relative or absolute. */
+	href?: string
+
+	/** Icon for the button. */
+	icon?: Node | IIconProps
+
+	/** Type attribute for HTML button element. Defaults to 'button'. */
+	type?: string
+
+	/** Click handler. */
+	onClick?: Function
+
+	/** Will be passed back with the on click. */
+	payload?: Record<string, any>
+
+	/** Component used to render anchor */
+	AnchorComponent?: Node
+}
+
+interface IButtonGroupProps {
+	/** The actions to be shown on tap/click */
+	actions: IButtonProps[]
+
+	/** DEPRECATED Set true to left align the menu */
+	isLeftAligned?: boolean
+
+	/** DEPRECATED Set true to right align the menu */
+	isRightAligned?: boolean
+
+	/** DEPRECATED Set true to align menu above button */
+	isBottomAligned?: boolean
+
+	/** Set the width of the menu. Helpful for longer text in buttons */
+	size?: 'medium' | 'large'
+
+	/** Adds text to the collapsed menu */
+	// NOTE: This should be required for accessibility
+	text?: string
+
+	/** Overrides the default icon */
+	icon?: any
+
+	/** Set true to make the button blue */
+	isSimple?: boolean
+
+	/** Set true to make the button smaller */
+	isSmall?: boolean
+
+	/** Set tot true makes the menu close when any action is selected */
+	closeOnSelectAction: boolean
+
+	/** Hide the icon entirely */
+	isTextOnly: boolean
+
+	/** Optional classname that applies to the button */
+	className?: string
+
+	onToggleContextMenuVisible?: Function
+}
+
+export interface ISplitButtonProps {
+	/** The main action readily surfaced to the user */
+	defaultAction: IButtonProps
+
+	/** All the secondary nested actions */
+	actions: IButtonProps[]
+
+	/*Sets the visual hierarchy of the button **/
+	kind: 'primary' | 'secondary'
+
+	/* Set true to fill the parent’s width **/
+	isFullWidth?: boolean
+
+	/* Sets the visual hierarchy of the button **/
+	isSmall?: boolean
+}
+
+const SplitButton = (props: ISplitButtonProps): React.ReactNode => {
+	const { defaultAction, actions, kind, isFullWidth, isSmall } = props
+	return (
+		<div className="split-button">
+			<Button
+				kind={kind}
+				className="split-button__default"
+				{...defaultAction}
+			/>
+			<Button
+				kind={kind}
+				className="split-button__actions"
+				icon={{ name: 'keyboard_arrow_down' }}
+			/>
+		</div>
+	)
+}
+
+SplitButton.defaultProps = {
+	isFullWidth: false,
+	isSmall: false
 }
 
 export default SplitButton
