0138f8d88fe8416ea33b014b41105c1a9b9144c9
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-07-03 09:36:20 -0600

    minor: hook up knobs and add more actions

--- packages/react-heartwood-components/src/components/SplitButton/SplitButton-story.js
+++ packages/react-heartwood-components/src/components/SplitButton/SplitButton-story.js
@@ -1,35 +1,60 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withKnobs, text } from '@storybook/addon-knobs/react'
 
 import SplitButton from './SplitButton'
 
-storiesOf('SplitButton', module)
+const actions = [
+	{
+		text: 'Another action'
+	},
+	{
+		text: 'Third action'
+	},
+	{
+		text: 'Yet another action'
+	}
+]
+
+const stories = storiesOf('SplitButton', module)
+
+stories.addDecorator(withKnobs)
+
+stories
 	.add('Primary', () => (
 		<SplitButton
-			defaultAction={{ text: 'Do the default action' }}
-			actions={[{ text: 'Thing two' }]}
+			defaultAction={{
+				text: text('defaultActionText', 'Do the default action')
+			}}
+			actions={actions}
 			kind="primary"
 		/>
 	))
 	.add('Primary Small', () => (
 		<SplitButton
-			defaultAction={{ text: 'Do the default action' }}
-			actions={[{ text: 'Thing two' }]}
+			defaultAction={{
+				text: text('defaultActionText', 'Do the default action')
+			}}
+			actions={actions}
 			kind="primary"
 			isSmall
 		/>
 	))
 	.add('Secondary', () => (
 		<SplitButton
-			defaultAction={{ text: 'Do the default action' }}
-			actions={[{ text: 'Thing two' }]}
+			defaultAction={{
+				text: text('defaultActionText', 'Do the default action')
+			}}
+			actions={actions}
 			kind="secondary"
 		/>
 	))
 	.add('Secondary Small', () => (
 		<SplitButton
-			defaultAction={{ text: 'Do the default action' }}
-			actions={[{ text: 'Thing two' }]}
+			defaultAction={{
+				text: text('defaultActionText', 'Do the default action')
+			}}
+			actions={actions}
 			kind="secondary"
 			isSmall
 		/>
