d0564e75c2af626b5f24b371cb72c714ba3bae1a
Brandon Minch <brandon@barbershop.io>
2019-07-08 10:24:43 -0600

    minor: clean up stories

--- packages/react-heartwood-components/src/components/RecordSelectionList/RecordSelectList-story.js
+++ packages/react-heartwood-components/src/components/RecordSelectionList/RecordSelectList-story.js
@@ -6,7 +6,6 @@ import { withKnobs, boolean, select } from '@storybook/addon-knobs/react'
 
 import { generateLocations } from '../../../.storybook/data/tableData'
 import RecordSelectionList from '../RecordSelectionList/RecordSelectionList'
-import RecordSelectionListItem from '../RecordSelectionList/RecordSelectionListItem'
 import Modal from '../Modal/Modal'
 import Button from '../Button/Button'
 import Card, { CardHeader, CardBody } from '../Card'
@@ -174,57 +173,3 @@ stories
 			</Modal.Body>
 		</Modal>
 	))
-
-// class WithModalExample extends Component<Props, State> {
-// 	state = {
-// 		isModalOpen: false
-// 	}
-
-// 	toggleModal = () => {
-// 		this.setState({ isModalOpen: !this.state.isModalOpen })
-// 	}
-
-// 	render() {
-// 		const locations = generateLocations({
-// 			amount: 1000
-// 		})
-
-// 		return (
-// 			<Fragment>
-// 				<Button
-// 					text={`Show me the list`}
-// 					onClick={() => this.toggleModal()}
-// 					kind="secondary"
-// 				/>
-// 				<Modal
-// 					isOpen={this.state.isModalOpen}
-// 					onRequestClose={this.toggleModal}
-// 				>
-// 					<RecordSelectionList
-// 						selectedIds={locations.map(loc => loc.id)}
-// 						loadRecords={async ({ limit, offset }) => {
-// 							// Artificial API wait time
-// 							await new Promise(resolve =>
-// 								setTimeout(() => {
-// 									resolve()
-// 								}, Math.random() * 1000)
-// 							)
-
-// 							return locations.slice(offset, offset + limit)
-// 						}}
-// 						renderRecord={record => (
-// 							<RecordSelectionListItem
-// 								id={record.id}
-// 								title={record.publicName}
-// 								subtitle={record.address}
-// 								icon={{ name: 'location', isLineIcon: true }}
-// 							/>
-// 						)}
-// 					/>
-// 				</Modal>
-// 			</Fragment>
-// 		)
-// 	}
-// }
-
-// stories.add('In modal', () => <WithModalExample />)
