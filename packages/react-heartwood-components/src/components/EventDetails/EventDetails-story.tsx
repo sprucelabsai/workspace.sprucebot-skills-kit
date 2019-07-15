048fbc2e66077f215217ac9a03beb015b2e8ae2c
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-07-15 14:48:01 -0600

    minor: remove bigcalendar styles and improve storybook story

--- packages/react-heartwood-components/src/components/EventDetails/EventDetails-story.tsx
+++ packages/react-heartwood-components/src/components/EventDetails/EventDetails-story.tsx
@@ -10,7 +10,58 @@ stories.add('Hello World', () => (
 	<Sidebar side="right" isCollapsible={false} isLarge>
 		<EventDetails
 			items={[
-				{ id: '1', component: 'button', componentProps: { text: 'Test' } }
+				{
+					id: 'guestInfo',
+					component: 'list',
+					componentProps: {
+						items: [
+							{
+								avatar:
+									'https://images.unsplash.com/photo-1542080681-b52d382432af?ixlib=rb-1.2.1&auto=format&fit=crop&w=96&h=96&q=80',
+								title: 'Alejandra Pollich',
+								subtitle: '(605) 230-5253',
+								contextMenu: {
+									icon: { name: 'edit', isLineIcon: true },
+									isSimple: true,
+									size: 'large',
+									actions: [
+										{
+											text: 'Edit guest'
+										},
+										{
+											text: 'Book for someone else'
+										}
+									]
+								}
+							},
+							{
+								icon: { name: 'note', isLineIcon: true },
+								title: 'Prefers products that aren’t tested on animals.',
+								subtitle: 'Caleigh Jerde, 4 months ago'
+							}
+						],
+						primaryAction: 'test'
+					}
+				},
+				{
+					id: 'dateAndTime',
+					component: 'list',
+					componentProps: {
+						items: [
+							{
+								icon: { name: 'calendar', isLineIcon: true },
+								title: 'Web, Nov 28, 2018',
+								subtitle: '11am–12:15pm',
+								actions: [
+									{
+										icon: { name: 'edit' },
+										kind: 'simple'
+									}
+								]
+							}
+						]
+					}
+				}
 			]}
 		/>
 	</Sidebar>
