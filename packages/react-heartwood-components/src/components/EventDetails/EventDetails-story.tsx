064c5632d306177099bd5fb73f184f13a964f6a6
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-07-19 08:35:36 -0600

    minor: add sidebar headers to stories

--- packages/react-heartwood-components/src/components/EventDetails/EventDetails-story.tsx
+++ packages/react-heartwood-components/src/components/EventDetails/EventDetails-story.tsx
@@ -3,8 +3,8 @@ import { storiesOf } from '@storybook/react'
 
 import EventDetails from './EventDetails'
 
-import { Sidebar } from '../Core'
-import Page from '../Page'
+import { Sidebar, SidebarHeader } from '../Core'
+// import Page from '../Page'
 
 import {
 	appointment,
@@ -17,34 +17,39 @@ import {
 const stories = storiesOf('EventDetails', module)
 
 // TODO: Turn off this decorator for specs. It obfuscates prop tables.
-stories.addDecorator(storyFn => (
-	<Page STORYBOOKwrap={false} sidebar={storyFn()} />
-))
+// stories.addDecorator(storyFn => (
+// 	<Page STORYBOOKwrap={false} sidebar={storyFn()} />
+// ))
 
 stories
 	.add('Booking: Appointment', () => (
 		<Sidebar side="right" isCollapsible={false} isLarge>
+			<SidebarHeader title="Appointment details" onClose={() => null} />
 			<EventDetails {...appointment} />
 		</Sidebar>
 	))
 	.add('Booking: Appointment has warning', () => (
 		<Sidebar side="right" isCollapsible={false} isLarge>
+			<SidebarHeader title="Appointment details" onClose={() => null} />
 			<EventDetails {...warningAppointment} />
 		</Sidebar>
 	))
 	.add('Booking: Past Appointment', () => (
 		<Sidebar side="right" isCollapsible={false} isLarge>
+			<SidebarHeader title="Appointment details" onClose={() => null} />
 			<EventDetails {...pastAppointment} />
 		</Sidebar>
 	))
 	.add('Scheduling: Lunch Break', () => (
 		<Sidebar side="right" isCollapsible={false} isLarge>
+			<SidebarHeader title="Lunch break" onClose={() => null} />
 			<EventDetails {...lunchBreak} />
 		</Sidebar>
 	))
 
 	.add('Scheduling: PTO', () => (
 		<Sidebar side="right" isCollapsible={false} isLarge>
+			<SidebarHeader title="PTO" onClose={() => null} />
 			<EventDetails {...ptoBlock} />
 		</Sidebar>
 	))
