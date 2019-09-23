52da063c87de3ca20ad6082d041c66c5e07b1fad
jtini <jeremytinianow@Jeremys-MacBook-Pro-2.local>
2019-09-23 12:52:26 -0600

    docs: add layout stories

--- packages/react-heartwood-components/src/components/Layout/Layout-story.js
+++ packages/react-heartwood-components/src/components/Layout/Layout-story.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs } from '@storybook/addon-knobs/react'
+import { select, withKnobs } from '@storybook/addon-knobs/react'
 import faker from 'faker'
 
 import Layout, { LayoutSection } from './index'
@@ -20,6 +20,50 @@ const generateCard = () => (
 )
 
 stories
+	.add('Spacing Utilities', () => {
+		const prefixSelect = select(
+			'side',
+			['all', 'horizontal', 'vertical', 'top', 'right', 'bottom', 'left'],
+			'all'
+		)
+		const prefixes = {
+			all: '',
+			horizontal: 'x',
+			vertical: 'y',
+			top: 't',
+			right: 'r',
+			bottom: 'b',
+			left: 'l'
+		}
+		return (
+			<div style={{ backgroundColor: 'rgba(49,95,204,0.5)' }}>
+				<div
+					className={`p${prefixes[prefixSelect]}-${select(
+						'spacing',
+						[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+						1
+					)}`}
+					style={{ width: 'auto', backgroundColor: 'rgba(49,95,204,0.5)' }}
+				>
+					<p style={{ backgroundColor: '#fff' }}>This is content</p>
+				</div>
+			</div>
+		)
+	})
+	.add('Container Classes', () => (
+		<div className="p-4" style={{ backgroundColor: 'rgba(49,95,204,0.5)' }}>
+			<div
+				className={`container--${select(
+					'container class',
+					['base', 'tight', 'wide'],
+					'base'
+				)}`}
+				style={{ backgroundColor: '#fff' }}
+			>
+				<p>This is the content</p>
+			</div>
+		</div>
+	))
 	.add('Primary Layout', () => (
 		<Layout>
 			<LayoutSection>{generateCard()}</LayoutSection>
