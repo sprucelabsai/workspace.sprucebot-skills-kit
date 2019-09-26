f4f4702b39ce46bcd2996073fe1566b8bd9743f7
taylorromero <me>
2019-09-26 15:31:18 -0600

    move to utilities

--- packages/spruce-skill-server/utilities/il8n.tsx
+++ packages/spruce-skill-server/utilities/il8n.tsx
@@ -1,5 +1,10 @@
+import { Location } from '../models/Location'
+
 /** Returns a string of hours and minutes. Ex: "4hr 30 min" */
-export const friendlyDuration = (seconds: number, lang?: any): string => {
+export const friendlyDuration = (
+	seconds: number,
+	location: Location
+): string => {
 	const minutes = Math.floor(seconds / 60)
 	const hours = Math.floor(minutes / 60)
 	const hrString: string | undefined = lang && lang.getText('hr')
@@ -19,11 +24,15 @@ export const friendlyDuration = (seconds: number, lang?: any): string => {
 }
 
 /** Converts a numnber of US cents into a formatted currency string. US Dollars by default. Ex: "$23.99" */
-export const friendlyCurrency = (
-	usCents: number,
-	locale?: string,
-	currency?: string
-): string => {
+export const friendlyCurrency = (cents: number, location: Location): string => {
+	const local = 'US/EN'
+	const currency = 'US_DOLLAR'
+
+	// TODO use location once it supports locale
+	// const locale = locatinon.locale
+	// TODO use location once it supports currency
+	// const currency = location.currency
+
 	const usDollars = usCents / 100
 	const friendlyCurrency = usDollars
 		.toLocaleString(locale || 'en-US', {
