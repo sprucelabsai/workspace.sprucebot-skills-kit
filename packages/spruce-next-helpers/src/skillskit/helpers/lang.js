22f839b9976975f8c18ace9820ca403ba9b4076f
Ricky Padilla <rickycpadilla@gmail.com>
2019-09-29 16:19:55 -0600

    Updated and moved `friendlyCurrency` and `friendlyDuration`

--- packages/spruce-next-helpers/src/skillskit/helpers/lang.js
+++ packages/spruce-next-helpers/src/skillskit/helpers/lang.js
@@ -1,3 +1,6 @@
+import * as moment from 'moment'
+import Dinero from 'dinero.js'
+
 export default {
 	lang: {},
 	overrides: {},
@@ -26,5 +29,74 @@ export default {
 		}
 
 		throw Error(`Translation missing key ${key}`)
+	},
+	/** Converts a numnber of cents into a formatted currency string. Ex: "$23.99" */
+	friendlyCurrency: cents => {
+		/*
+    TODO:
+    const { currency, locale } = this.ctx.location
+    */
+		const currency = 'USD'
+		const locale = 'en-US'
+		const friendlyCurrency = Dinero({ amount: cents, precision: 2, currency })
+			.setLocale(locale)
+			.toFormat('$0,0.00')
+			.replace('.00', '')
+		return friendlyCurrency
+	},
+
+	/** Returns a duration string of largest time increments from numer of seconds. Ex: `friendlyDuration(3452626, 2)` returns "1mo 8d" */
+	friendlyDuration(seconds, largest, showSeconds) {
+		const getDurationLabel = label => {
+			try {
+				return this.getText(label)
+			} catch {
+				return label
+			}
+		}
+		const secondsDuration = moment.duration(seconds, 'seconds')
+		const years = {
+			value: secondsDuration.years(),
+			label: getDurationLabel('yr')
+		}
+		const months = {
+			value: secondsDuration.months(),
+			label: getDurationLabel('mo')
+		}
+		const days = {
+			value: secondsDuration.days(),
+			label: getDurationLabel('d')
+		}
+		const hours = {
+			value: secondsDuration.hours(),
+			label: getDurationLabel('hr')
+		}
+		const minutes = {
+			value: secondsDuration.minutes(),
+			label: getDurationLabel('min')
+		}
+		let allDurations = [years, months, days, hours, minutes]
+		if (showSeconds) {
+			allDurations.push({
+				value: secondsDuration.seconds(),
+				label: getDurationLabel('sec')
+			})
+		}
+		let durationCount = 0
+		let friendlyDuration = ''
+		for (let i = 0; i < allDurations.length; i++) {
+			const duration = allDurations[i]
+			if (i !== allDurations.length - 1 && duration.value === 0) {
+				continue
+			}
+			friendlyDuration += `${durationCount ? ' ' : ''}${duration.value}${
+				duration.label
+			}`
+			durationCount++
+			if (durationCount === (largest || 2)) {
+				break
+			}
+		}
+		return friendlyDuration
 	}
 }
