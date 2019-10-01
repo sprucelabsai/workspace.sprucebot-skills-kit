2d5c6b8a661f1bd8939b86f19c4e6226e8da6772
Ricky Padilla <rickycpadilla@gmail.com>
2019-10-01 10:15:56 -0600

    [SDEV3-1485] Added `friendlyCurrency` and `friendlyDuration` to `spruce-skill-server`

--- packages/spruce-skill-server/utilities/lang.ts
+++ packages/spruce-skill-server/utilities/lang.ts
@@ -1,14 +1,20 @@
 import { SpruceSkillUtility } from '../index'
 import { ISpruceContext } from '../interfaces/ctx'
 import { lang } from '@sprucelabs/spruce-next-helpers'
+import { ICoreLocation } from '@sprucelabs/spruce-types'
 
 export default class Lang extends SpruceSkillUtility<ISpruceContext> {
 	public constructor(options: any) {
-		debugger
 		super(options)
 		lang.configure(options.config.dir)
 	}
 	public getText(key: string, context: Record<string, any> = {}): string {
 		return lang.getText(key, context)
+	}
+	public friendlyCurrency(cents: number, location: ICoreLocation): string {
+		return lang.friendlyCurrency(cents, location)
+	}
+	public friendlyDuration(seconds: number, location: ICoreLocation): string {
+		return lang.friendlyDuration(seconds, location)
 	}
 }
