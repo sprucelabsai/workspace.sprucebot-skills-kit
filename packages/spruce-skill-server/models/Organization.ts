8ea7008fe8430076f9aaca70e56babf012fa1bbb
liquidg3 <taylorromero@taysmacbook.local>
2019-10-14 15:11:01 -0600

    make slug on org available in skills

--- packages/spruce-skill-server/models/Organization.ts
+++ packages/spruce-skill-server/models/Organization.ts
@@ -25,12 +25,16 @@ export class Organization extends SpruceCoreModel<Organization> {
 		},
 		name: {
 			type: DataTypes.STRING
+		},
+		slug: {
+			type: DataTypes.STRING
 		}
 	}
 
 	public id!: string
 	public name!: string
 	public Locations?: Location[] | null
+	public slug!: string
 
 	public static associate(models: ISpruceModels): void {
 		this.hasMany(models.Location, { constraints: false })
