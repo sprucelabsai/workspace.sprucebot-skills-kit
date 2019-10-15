ca33ededd9ea77a6d3eb6e9f56c76fa22bb035ad
liquidg3 <taylorromero@taysmacbook.local>
2019-10-15 10:37:04 -0600

    fixes

--- packages/spruce-next-helpers/src/skillskit/helpers/errors.ts
+++ packages/spruce-next-helpers/src/skillskit/helpers/errors.ts
@@ -1,7 +1,7 @@
 import get from 'ts-get'
 
 export default {
-	/** pass an error from a try/catch atfer an api call (gql) and I'll pull out the good error messages */
+	/** pass an error from a try/catch after an api call (gql) and I'll pull out the good error messages */
 	getFriendlyReasons(err: any, fallback?: string) {
 		const errorMessages: string[] = get(
 			err,
@@ -13,6 +13,6 @@ export default {
 			errorMessages.push(fallback)
 		}
 
-		return errorMessages
+		return errorMessages.filter(message => !!message)
 	}
 }
