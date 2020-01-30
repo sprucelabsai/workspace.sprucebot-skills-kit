59929d5f75225a2aae642431c00a4adc88607385
Adam Goth <adamgoth@adamgoth.local>
2020-01-30 08:57:48 -0700

    Add query for user id but no org id

--- packages/spruce-skill/config/auth.ts
+++ packages/spruce-skill/config/auth.ts
@@ -10,6 +10,32 @@ const gql = (options: {
 }): string => `
 {
 	${
+		options.userId && !options.organizationId
+			? `
+	User (
+		id: "${options.userId}"
+	) {
+		id
+		firstName
+		lastName
+		UserGroups {
+			edges {
+				node {
+					Group {
+						name
+					}
+					Job {
+						name
+						isDefault
+						role
+					}
+				}
+			}
+		}
+	}`
+			: ``
+	}
+	${
 		options.userId && options.organizationId
 			? `
 	User (
