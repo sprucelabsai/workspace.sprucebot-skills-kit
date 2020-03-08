4de3ecc5e5c7f2208d490c4f18907c9af1625495
liquidg3 <me@taylorrome.ro>
2020-03-08 14:56:21 -0600

    proposal

---
+++ packages/spruce-types/src/schema/fieldTypes/select.ts
@@ -0,0 +1,18 @@
+import { ISpruceSchemaFieldBase } from '../schema'
+import { SpruceSchemaFieldType } from '../fieldTypes'
+
+export interface ISpruceSchemaFieldTypeSelectChoice {
+	/**  machine readable way to identify this choice */
+	id: string
+	/** human readable label for when selecting a choice */
+	label?: string
+}
+export default interface ISpruceSchemaFieldTypeSelect
+	extends ISpruceSchemaFieldBase {
+	type: SpruceSchemaFieldType.Select
+	value?: string
+	defaultValue?: string
+	options?: {
+		choices: ISpruceSchemaFieldTypeSelectChoice[]
+	}
+}
